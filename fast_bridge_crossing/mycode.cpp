75392e0293c1cc774b176f452f64960f3dd1a860
yichuanshuzi <chenboshuo.hi@163.com>
2019-01-06 21:58:01 +0800

    函数写完了

--- fast_bridge_crossing/mycode.cpp
+++ fast_bridge_crossing/mycode.cpp
@@ -3,7 +3,7 @@
  */
 #include "stdio.h"
 
-int len_a,len_b;//设为全局变量方便函数调用
+int a[1000], b[1000],len_a,len_b;//设为全局变量方便函数调用
 
 int max(int a, int b){
   if(a > b){
@@ -30,12 +30,53 @@ void bubble_sort(int *p, int len) {
   }
 }
 
+/**
+ * 删除前number项并返回长度
+ * @param  a      数组名
+ * @param  len    数组长度
+ * @param  number 要删掉的项数
+ * @return        数组长度
+ */
+int remove(int *a,int len,int number) {
+  for (int i = 0; i < len-number; i++) {
+    a[i] = a[i+number];
+  }
+  return len - number;
+}
+
 int across_river(int *a, int *b,int len_a, int len_b) {
   if (len_a == 1) {
     *(b+len_b) = *a;
-    return *a;
+    len_a = remove(a,len_a,1);
+    len_b++;
+    bubble_sort(b,len_b);
+    return *(b);
   } else {
     *(b+len_b) = *a;
-    *(b+len_b) = *b;
+    *(b+len_b+1) = *(a+1);
+    len_a = remove(a,len_a, 2);
+    len_b += 2;
+    bubble_sort(b,len_b);
+    return *(b+1);// 排序后,b的前两项为过河的人,第二个为时间较长的人
   }
+}
+
+int go_back(int *a, int *b,int len_a, int len_b){
+  *(a+len_a) = *b;
+  bubble_sort(a,len_a);
+  len_b = remove(b,len_b, 1);
+  return *a;
+}
+
+int main(int argc, char const *argv[]) {
+  int count;
+  scanf("%d\n", count);
+  while (count--) {
+    // 输入
+    scanf("%d\n", &len_a);
+    for (int i = 0; i < len_a; i++) {
+      scanf("%d\n", a+i);
+    }
+  }
+  return 0;
 }
