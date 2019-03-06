bb1c8c03ac19bd2ce6ba57a3f32eccf6d935a5d9
yichuanshuzi <chenboshuo.hi@163.com>
2019-03-06 18:39:15 +0800

    leetcode效率最高的代码(代码风骚,效率恐怖) https://leetcode.com/explore/learn/card/recursion-i/250/principle-of-recursion/1681/discuss/248445/C++-100-4ms-using-recursion /**  * Swap Nodes in Pairs  * Given a linked list, swap every two adjacent nodes and return its head.  *  * You may not modify the values in the list's nodes, only nodes itself may be changed.  * Example:  * Given 1->2->3->4, you should return the list as 2->1->4->3.  */

--- leetcode/explore_recursion/swap_nodes_in_pairs.cpp
+++ leetcode/explore_recursion/swap_nodes_in_pairs.cpp
@@ -5,17 +5,17 @@
  * You may not modify the values in the list's nodes, only nodes itself may be changed.
  * Example:
  * Given 1->2->3->4, you should return the list as 2->1->4->3.
+ * solution: https://leetcode.com/explore/learn/card/recursion-i/250/principle-of-recursion/1681/discuss/248445/C++-100-4ms-using-recursionhttps://leetcode.com/explore/learn/card/recursion-i/250/principle-of-recursion/1681/discuss/248445/C++-100-4ms-using-recursion
  */
 struct ListNode {
-     int val;
-     struct ListNode *next;
- };
-struct ListNode* swapPairs(struct ListNode* head) {
-  if(!head->next->next){
-    return 0;
-  }
-  struct ListNode* new_next = head->next->next;
-  head->next->next = &head;
-  head->next = new_next;
-  swapPairs(head->next);
+ int val;
+ struct ListNode *next;
+};
+ListNode* swapPairs(ListNode* head) {
+	if (!head || !head->next) return head; //if the next two nodes are : something->null or just null : need no swap
+	ListNode* second = head->next;
+	ListNode* temp = second->next;
+	second->next = head;
+	head->next = swapPairs(temp);
+	return second;
 }
