0e32ce9b0377f6c4a6e1748a3285e3117045e9eb
yichuanshuzi <chenboshuo.hi@163.com>
2019-03-09 19:50:16 +0800

    121测试通过,死在11上

--- leetcode/0009-PalindromeNumber/mycode.c
+++ leetcode/0009-PalindromeNumber/mycode.c
@@ -8,9 +8,9 @@ bool isPalindrome(int x) {
         s[i++] = x % 10;
         x /= 10;
     }while(x);
-    for(int *p=s,*q=s+i; p!=q; p++,q--){
+    for(int *p=s,*q=s+i-1; p!=q; p++,q--){
         if (*p != *q) return false;
     }
     return true;
 }
-// wrong answer
+// 	Runtime Error
