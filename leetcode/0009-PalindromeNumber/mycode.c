608235a9d92ea84f87ea1a0091e0f715f76dcf5e
yichuanshuzi <chenboshuo.hi@163.com>
2019-03-09 19:47:43 +0800

    9. [Palindrome Number](https://leetcode.com/problems/palindrome-number/)
    Determine whether an integer is a palindrome. An integer is a palindrome 
    when it reads the same backward as forward.
    
    Example 1:
    ```
    Input: 121
    Output: true
    ```
    
    Example 2:
    ```
    Input: -121
    Output: false
    
    Explanation: From left to right, it reads -121. From right to left, it 
    becomes 121-. Therefore it is not a palindrome.
    ```
    ```
    Example 3:
    
    Input: 10
    Output: false
    Explanation: Reads 01 from right to left. Therefore it is not a 
    palindrome.
    ```
    
    
    Coud you solve it without converting the integer to a string?

---
+++ leetcode/0009-PalindromeNumber/mycode.c
@@ -0,0 +1,16 @@
+bool isPalindrome(int x) {
+    if(x < 0) return false;
+
+    int i = 0;
+    int s[20];
+
+    do{
+        s[i++] = x % 10;
+        x /= 10;
+    }while(x);
+    for(int *p=s,*q=s+i; p!=q; p++,q--){
+        if (*p != *q) return false;
+    }
+    return true;
+}
+// wrong answer
