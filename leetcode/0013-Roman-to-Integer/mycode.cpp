c9be6da7248f208806cf3ef4beb18221895bfc89
yichuanshuzi <chenboshuo.hi@163.com>
2019-03-10 16:20:29 +0800

    变换思路 40ms,29.6MB; https://leetcode.com/problems/roman-to-integer/discuss/6857/Simple-c-code-in-18ms

--- leetcode/0013-Roman-to-Integer/mycode.cpp
+++ leetcode/0013-Roman-to-Integer/mycode.cpp
@@ -1,28 +1,34 @@
+/**
+ * reference:
+ * https://leetcode.com/problems/roman-to-integer/discuss/6857/Simple-c-code-in-18ms
+ *
+ */
 int romanToInt(char* s) {
     int ans=0;
     int a=0;
-    int num;
-    for(int i=0;s[i];i++){
+    int num=0;
+    int n[256]={0};
+
+    n['I'] = 1;
+    n['V'] = 5;
+    n['X'] = 10;
+    n['L'] = 50;
+    n['C'] = 100;
+    n['D'] = 500;
+    n['M'] = 1000;
+    for(;*s;s++){
         a = num;
-        switch(s[i]){
-            case 'I': num = 1; break;
-            case 'V': num = 5; break;
-            case 'X': num = 10; break;
-            case 'L': num = 50; break;
-            case 'C': num = 100; break;
-            case 'D': num = 500; break;
-            case 'M': num = 1000; break;
-        }
+        num = n[*s];
         if(a<num){
-            num -= 2*a;
+            ans -= 2*a;
         }
         ans += num ;
     }
     return ans;
 }
 /**
- * Runtime: 44 ms,
- * faster than 64.63% of C online submissions for Roman to Integer.
- * Memory Usage: 29.5 MB,
- * less than 86.96% of C online submissions for Roman to Integer.
+ * Runtime: 40 ms,
+ * faster than 87.77% of C online submissions for Roman to Integer.
+ * Memory Usage: 29.6 MB,
+ * less than 80.43% of C online submissions for Roman to Integer.
  */
