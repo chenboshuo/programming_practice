a54f18eace6e7c73d03d068937b3fc6dc99d08c5
cbs <chenboshuo.hi@163.com>
2019-03-14 10:57:16 +0800

    Runtime: 4 ms(%54.89);Memory Usage: 7 MB(%24.00)

--- leetcode/0070-Climbing-Stairs/mycode.c
+++ leetcode/0070-Climbing-Stairs/mycode.c
@@ -1,6 +1,17 @@
+int memo[256] = {0};
+
 int climbStairs(int n) {
-    if(n == 0 || n == 1) return 1;
-    return climbStairs(n - 1) +climbStairs(n - 2);
+     if(memo[n]){
+        return memo[n];
+    }
+    if(n == 0 || n == 1){
+        memo[0] = 1;
+        memo[1] = 1;
+        return 1;
+    }
+    memo[n] = climbStairs(n - 1) +climbStairs(n - 2);
+    return memo[n];
 }
 
-// Time Limit Exceeded
+// Runtime: 4 ms, faster than 54.89% of C online submissions for Climbing Stairs.
+// Memory Usage: 7 MB, less than 24.00% of C online submissions for Climbing Stairs.
