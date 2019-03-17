c13de042ea4a3493fa8490160f59c54bb5203a89
yichuanshuzi <chenboshuo.hi@163.com>
2019-03-17 21:49:53 +0800

    又一次错误提交

--- leetcode/0020-Valid-Parentheses/mycode.c
+++ leetcode/0020-Valid-Parentheses/mycode.c
@@ -1,28 +1,19 @@
 bool isValid(char* s) {
-    int a = 0;//()
-    int b = 0;//[]
-    int c = 0;//{}
+    int brackets[100];
+    int *p = brackets+1;
     for(int i=0; *(s+i); i++){
         switch(*(s+i)){
-                case'(': a+=1; break;
-                case')': a-=1; break;
-                case'[': b+=1; break;
-                case']': b-=1; break;
-                case'{': c+=1; break;
-                case'}': c-=1; break;
+                case'(': *p=1; p++; break;
+                case')': if(*(p-1) == 1) {p--;} else{return false;}break;
+                case'[': *p=2; p++; break;
+                case']': if(*(p-1) == 2) {p--;} else{return false;} break;
+                case'{': *p = 3; p++; break;
+                case'}': if(*(p-1) == 3) {p--;} else{return false;} break;
         }
-    }
-    if(a || b || c){
-        return false;
     }
     return true;
 }
-// Input:
-// "([)]"
-
-// Output:
-// true
-
-// Expected:
-// false
-// 想法太天真
+// Runtime Error Message:
+// AddressSanitizer: stack-buffer-underflow on address 0x7ffc06f490ec at pc 0x000000401996 bp 0x7ffc06f490c0 sp 0x7ffc06f490b8
+// Last executed input:
+// "]"
