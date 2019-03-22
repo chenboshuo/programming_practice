c00361ad8afa71af54f9e564d4f70f299fa17c4f
yichuanshuzi <chenboshuo.hi@163.com>
2019-03-22 20:12:22 +0800

    a classic error

--- leetcode/0021-Merge-Two-Sorted-Lists/mycode.c
+++ leetcode/0021-Merge-Two-Sorted-Lists/mycode.c
@@ -12,32 +12,43 @@ struct ListNode {
 };
 
 struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
-  if(!l1 && !l2){
-    return NULL;
-  }else if(!l1 && l2){
-    return l2;
-  }else if(l1 && !l2){
-    return l1;
-  }
-    struct ListNode *next1 = l1->next;
+
+
+    if(!l1 && !l2){
+        return NULL;
+    }else if(!l1 && l2){
+        return l2;
+    }else if(l1 && !l2){
+        return l1;
+    }
+
+    struct ListNode *next1 = l1 -> next;
     struct ListNode *next2 = l2 -> next;
 
-  if(l1->val <= l2->val){
-    l1->next = l2;
-    l2->next = mergeTwoLists(next1, next2);
-    return l1;
-  }else{
-    l2->next = l1;
-    l1->next = mergeTwoLists(next1, next2);
-    return l2;
-  }
+
+    if(l1->val <= l2->val){
+        if(l2 <= next1){
+            l1 -> next = l2;
+        }else{
+            l1 -> next = mergeTwoLists(next1,l2);
+        }
+        return l1;
+    }else{
+        if(l1 <= next2){
+            l2 -> next = l1;
+        }else{
+             l2 -> next = mergeTwoLists(l1,next2);
+        }
+        return l2;
+    }
 }
 
-// https://leetcode.com/submissions/detail/216511959/
-// Input:
+// Finished
+// Runtime: 4 ms
+// Your input
 // [5]
-// [1,2,4]
-// Output:
-// [1,5,2,4]
-// Expected:
-// [1,2,4,5]
+// [1,3,4]
+// Output
+// [1,5]
+// Expected
+// [1,3,4,5]
