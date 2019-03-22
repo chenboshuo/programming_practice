ac347683465a2f0d0feeae7ba37cec866c19bd44
yichuanshuzi <chenboshuo.hi@163.com>
2019-03-22 20:26:15 +0800

    还有问题

--- leetcode/0021-Merge-Two-Sorted-Lists/mycode.c
+++ leetcode/0021-Merge-Two-Sorted-Lists/mycode.c
@@ -13,7 +13,6 @@ struct ListNode {
 
 struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
 
-
     if(!l1 && !l2){
         return NULL;
     }else if(!l1 && l2){
@@ -24,20 +23,21 @@ struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
 
     struct ListNode *next1 = l1 -> next;
     struct ListNode *next2 = l2 -> next;
-
+    if(!next1) next1 = &l1;
+    if(!next2) next2 = &l2;
 
     if(l1->val <= l2->val){
-        if(l2 <= next1){
+        if(l2 -> val <= next1 -> val){
             l1 -> next = l2;
         }else{
             l1 -> next = mergeTwoLists(next1,l2);
         }
         return l1;
     }else{
-        if(l1 <= next2){
-            l2 -> next = l1;
+        if(l1 -> val >= next2 -> val){
+            l2 -> next = mergeTwoLists(l1,next2);
         }else{
-             l2 -> next = mergeTwoLists(l1,next2);
+            l2 -> next = l1;
         }
         return l2;
     }
@@ -49,6 +49,17 @@ struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
 // [5]
 // [1,3,4]
 // Output
-// [1,5]
+// [1,3,4,5]
 // Expected
 // [1,3,4,5]
+
+// Wrong Answer
+// https://leetcode.com/submissions/detail/216759209/
+// Details
+// Input
+// [1,2,4]
+// [1,3,4]
+// Output
+// [1,1,3,4]
+// Expected
+// [1,1,2,3,4,4]
