f4a4336d11bbf96b7bd30caa25f2d1d0d88d2bd2
yichuanshuzi <chenboshuo.hi@163.com>
2019-04-27 08:16:31 +0800

    Runtime: 60 ms (80.85%) Python3 Memory Usage: 14.9 MB, (5.43%)  Python3

--- leetcode/0026-remove-duplicates-from-sorted-array/code.py
+++ leetcode/0026-remove-duplicates-from-sorted-array/code.py
@@ -1,5 +1,7 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+          return 0
         i = 0
         j = 0
         for _ in nums:
@@ -10,14 +12,9 @@ class Solution:
             nums[i] = nums[j]
             j += 1
 
-        return j-i-1
+        return i + 1
 
-# Wrong Answer
-# Details
-# Playground Debug
-# Input
-# [1,1,2]
-# Output
-# [1]
-# Expected
-# [1,2]
+# Runtime: 60 ms, 
+# faster than 80.85% of Python3 online submissions for Remove Duplicates from Sorted Array.
+# Memory Usage: 14.9 MB,
+# less than 5.43% of Python3 online submissions for Remove Duplicates from Sorted Array.
