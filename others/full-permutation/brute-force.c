911d933e022da7eed03ee0db78e90fd5846987ff
yichuanshuzi <chenboshuo.hi@163.com>
2019-04-30 21:42:31 +0800

    死因: 头指针的位置难道想变就变吗?

--- others/full-permutation/brute-force.c
+++ others/full-permutation/brute-force.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-void print(int n){
-  if (n == 1) {
-    printf("%d ", 1);
+
+/**
+ * 代替for循环
+ * @param a   目标数组
+ * @param len 数组长度
+ * @param n   全排列的目标值
+ */
+void array(int* a, int len,int n){
+  if (len == 0) {
+    for(int i = 0; i < n; ++i){
+      printf("%d ", a[i]);
+    }
     return;
   }
-  print(n-1);
-  printf("%d\n", n);
-  printf("%d ", n);
-  print(n-1);
+  for(*a = 1; *a <= n; (*a)++){
+    array(a+1,len-1,n);
+  }
+  return;
 }
 int main(int argc, char const *argv[]) {
   int n;
   // scanf("%d", &n);
-  n = 3;
-  int *a = (int*) malloc(sizeof(int) * n);
-  print(n);
+  n = 1;
+  int *a = (int*) malloc(sizeof(int) * (n + 1));
+  array(a,n,n);
   return 0;
 }
 /**
