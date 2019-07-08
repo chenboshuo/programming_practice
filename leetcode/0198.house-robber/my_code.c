ff33fdb19e06bfce186b36fa2f1335843db7e047
yichuanshuzi <chenboshuo.hi@163.com>
2019-07-08 20:36:13 +0800

    利用数组进行存储. 4ms(74.43%),7MB(54.47%)

--- leetcode/0198.house-robber/my_code.c
+++ leetcode/0198.house-robber/my_code.c
@@ -2,27 +2,43 @@
  * @lc app=leetcode id=198 lang=c
  *
  * [198] House Robber
+ * reference:
+ * https://leetcode.com/problems/house-robber/discuss/156523/From-good-to-great.-How-to-approach-most-of-DP-problems.
  */
 
+#include <stdlib.h>
 #define max(a, b) (a) > (b) ? (a) : (b)
+int *memo;
+int solve(int *nums, int numsSize);
+
 int rob(int *nums, int numsSize) {
   if (numsSize == 0) {
     return 0;
   }
-  if (numsSize == 1) {
-    return *nums;
+  memo = (int *)malloc(sizeof(int) * (numsSize + 1));
+  memo[0] = 0;
+  memo[1] = *nums;
+  for (int i = 2; i < numsSize + 1; ++i) {
+    memo[i] = -1;
   }
-  if (numsSize == 2) {
-    return max(*nums, *(nums + 1));
+  if (numsSize > 1) {
+    memo[2] = max(*nums, *(nums + 1));
   }
-  int sum1 = *nums + rob(nums + 2, numsSize - 2);
-  int sum2 = rob(nums + 1, numsSize - 1);
-  return max(sum1, sum2);
+  return solve(nums, numsSize);
+}
+
+int solve(int *nums, int numsSize) {
+  if (memo[numsSize] > -1) {
+    return memo[numsSize];
+  }
+  int sum1 =
+      nums[numsSize - 1] + solve(nums, numsSize - 2); // 头指针不能随便动了
+  int sum2 = solve(nums, numsSize - 1);
+  memo[numsSize] = max(sum1, sum2);
+  return memo[numsSize];
 }
 /*
-https://leetcode.com/submissions/detail/226865587/
-56 / 69 test cases passed.
-Time Limit Exceeded
-Last executed input:
-[114,117,207,117,235,82,90,67,143,146,53,108,200,91,80,223,58,170,110,236,81,90,222,160,165,195,187,199,114,235,197,187,69,129,64,214,228,78,188,67,205,94,205,169,241,202,144,240]
+https://leetcode.com/submissions/detail/241694540/
+Runtime: 4 ms, faster than 74.43% of C online submissions for House Robber.
+Memory Usage: 7 MB, less than 53.47% of C online submissions for House Robber.
  */
