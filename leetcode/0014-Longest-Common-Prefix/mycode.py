5a4f3873c6b350311f90acef247ce0f08b5737bc
yichuanshuzi <chenboshuo.hi@163.com>
2019-07-29 18:12:35 +0800

    逻辑错误:一个字符的话没有返回值

--- leetcode/0014-Longest-Common-Prefix/mycode.py
+++ leetcode/0014-Longest-Common-Prefix/mycode.py
@@ -2,8 +2,10 @@ class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if not strs:
           return ''
-        prefix = ''
         n = min([len(i) for i in strs]) + 1
+        if n == 1:
+          return ""
+        prefix = ''
         for i in range(0,n):
           _ = strs[0][i]
           for str in strs:
@@ -11,12 +13,10 @@ class Solution:
               return prefix
           else:
             prefix += _
-
-
 '''
 https://leetcode.com/submissions/detail/247216397/
 Runtime Error Message:
-Line 8: IndexError: string index out of range
+Line 10: IndexError: string index out of range
 Last executed input:
-[""]
+["a"]
 '''
