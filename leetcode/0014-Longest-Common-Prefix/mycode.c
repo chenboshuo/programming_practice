b39f11d04607c1c80f78db52148bf85a731a4efb
yichuanshuzi <chenboshuo.hi@163.com>
2019-07-30 16:25:04 +0800

    4ms(72.16%),7.1MB(47.71%) 增加变量提高速度的C代码

--- leetcode/0014-Longest-Common-Prefix/mycode.c
+++ leetcode/0014-Longest-Common-Prefix/mycode.c
@@ -2,19 +2,18 @@ char *longestCommonPrefix(char **strs, int strsSize) {
   if (strsSize <= 0) {
     return "";
   }
-  char *prefix = strs[0]; // 指针放在第一个字符串的首位
-  for (; *strs[0]; ++strs[0]) {
+  for (int j = 0; strs[0][j]; ++j) {
     for (int i = 1; i < strsSize; ++i) {
-      if (*(strs[i]++) != *strs[0]) { // *str[i] 排除都是'\0'
-        *strs[0] = '\0';
-        return prefix;
+      if (strs[i][j] != strs[0][j]) { // *str[i] 排除都是'\0'
+        strs[0][j] = '\0';
+        return strs[0];
       }
     }
   }
-  return prefix;
+  return strs[0];
 }
 
-// https://leetcode.com/submissions/detail/247483108/
-// Runtime: 8 ms, faster than 14.20% of C online submissions for Longest Common
-// Prefix. Memory Usage: 7 MB, less than 80.73% of C online submissions for
+// https://leetcode.com/submissions/detail/247484304/
+// Runtime: 4 ms, faster than 72.16% of C online submissions for Longest Common
+// Prefix. Memory Usage: 7.1 MB, less than 47.71% of C online submissions for
 // Longest Common Prefix.
