c51635183b0bec8102fc78b108aa3d5ec2bd4d5a
yichuanshuzi <chenboshuo.hi@163.com>
2019-07-31 17:37:20 +0800

    0ms(100%),7.3MB(68.69%) 稍微优化了一点

--- leetcode/0027- Remove-Element/mycode.c
+++ leetcode/0027- Remove-Element/mycode.c
@@ -1,14 +1,14 @@
 int removeElement(int *nums, int numsSize, int val) {
-  int *p = nums;
-  int *q = nums;
-  for (int i = 0; i < numsSize; ++i, ++q) {
-    if (*q != val) {
-      *p++ = *q;
+  int i, j = 0;
+  for (i = 0; i < numsSize; ++i) {
+    if (nums[i] != val) {
+      nums[j++] = nums[i];
     }
   }
-  return numsSize - (q - p);
+  return numsSize - (i - j);
 }
-// https://leetcode.com/submissions/detail/247771434/
+
+// https://leetcode.com/submissions/detail/247772609/
 // Runtime: 0 ms, faster than 100.00% of C online submissions for Remove
-// Element. Memory Usage: 7.3 MB, less than 36.36% of C online submissions for
+// Element. Memory Usage: 7.2 MB, less than 68.69% of C online submissions for
 // Remove Element.
