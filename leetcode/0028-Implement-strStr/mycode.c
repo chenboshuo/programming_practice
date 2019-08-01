4bf1e969359b20ad36b3f2dea9c4fc10c83dadbf
yichuanshuzi <chenboshuo.hi@163.com>
2019-08-01 17:31:48 +0800

    1300ms(13.85%),7.2MB(62.89%) 纯粹暴力破解

--- leetcode/0028-Implement-strStr/mycode.c
+++ leetcode/0028-Implement-strStr/mycode.c
@@ -4,7 +4,7 @@ int strStr(char *haystack, char *needle) {
   }
   int j;
   for (int i = 0; haystack[i]; ++i) {
-    for (j = 0; haystack[i + j] == needle[j]; ++j) {
+    for (j = 0; haystack[i + j] == needle[j] && haystack[i + j]; ++j) {
       ;
     }
     if (!needle[j]) { // needle[j] == '\0' 代表遍历完成
@@ -13,11 +13,9 @@ int strStr(char *haystack, char *needle) {
   }
   return -1;
 }
+}
 
-// https://leetcode.com/submissions/detail/248043197/
-//
-// Runtime Error
-
-// Input
-// "a"
-// "a"
+// https://leetcode.com/submissions/detail/248044156/
+// Runtime: 1300 ms, faster than 13.85% of C online submissions for Implement
+// strStr(). Memory Usage: 7.2 MB, less than 62.89% of C online submissions for
+// Implement strStr().
