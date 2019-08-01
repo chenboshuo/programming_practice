389d74ac58ffdfca3596f4ce92f5593145845be1
yichuanshuzi <chenboshuo.hi@163.com>
2019-08-01 19:41:11 +0800

    尝试跳的步长更大,结果溢出了

--- leetcode/0028-Implement-strStr/mycode.c
+++ leetcode/0028-Implement-strStr/mycode.c
@@ -1,21 +1,20 @@
-int strStr(char *haystack, char *needle) {
-  if (!*needle) { // 处理两个空字符串的特殊情况
+int strStr(char * haystack, char * needle){
+  if(!*needle){ // 处理两个空字符串的特殊情况
     return 0;
   }
   int j;
-  for (int i = 0; haystack[i]; ++i) {
-    for (j = 0; haystack[i + j] == needle[j] && haystack[i + j]; ++j) {
+  for(int i=0; haystack[i];i+=j+1){
+    for(j=0; haystack[i+j] == needle[j] && haystack[i+j];++j ){
       ;
     }
-    if (!needle[j]) { // needle[j] == '\0' 代表遍历完成
+    if(!needle[j]){ // needle[j] == '\0' 代表遍历完成
       return i;
     }
   }
   return -1;
 }
-}
 
-// https://leetcode.com/submissions/detail/248044156/
-// Runtime: 1300 ms, faster than 13.85% of C online submissions for Implement
-// strStr(). Memory Usage: 7.2 MB, less than 62.89% of C online submissions for
-// Implement strStr().
+// https://leetcode.com/submissions/detail/248045327/
+// Last executed input
+// "aaa"
+// "aaaa"
