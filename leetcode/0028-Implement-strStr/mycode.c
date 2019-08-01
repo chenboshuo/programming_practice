899e6f1ec35db7ce9807140c88fb250dbf71651c
yichuanshuzi <chenboshuo.hi@163.com>
2019-08-01 17:19:04 +0800

    忽略目标字符串比要查找的字符串短的情况

--- leetcode/0028-Implement-strStr/mycode.c
+++ leetcode/0028-Implement-strStr/mycode.c
@@ -1,23 +1,25 @@
-int strStr(char * haystack, char * needle){
+int strStr(char *haystack, char *needle) {
+  if (!*needle) { // 处理两个空字符串的特殊情况
+    return 0;
+  }
   int j;
-  for(int i=0; haystack[i];++i){
-    for(j=0; haystack[i] == needle[j];++j ){
+  for (int i = 0; haystack[i]; ++i) {
+    for (j = 0; haystack[i] == needle[j]; ++j) {
       ;
     }
-    if(!needle[j]){ // needle[j] == '\0' 代表遍历完成
+    if (!needle[j]) { // needle[j] == '\0' 代表遍历完成
       return i;
     }
   }
   return -1;
 }
-
-// https://leetcode.com/submissions/detail/248040331/
-// Wrong Answer
+//
+// https://leetcode.com/submissions/detail/248042107/
 //
 // Input
-// ""
-// ""
+// "aaa"
+// "aaaa"
 // Output
-// -1
-// Expected
 // 0
+// Expected
+// -1
--- leetcode/0415-add-strings/mycode.c
+++ leetcode/0415-add-strings/mycode.c
@@ -1,24 +1,22 @@
-#define max(a,b) (a) > (b) ? (a) : (b)
-#define min(a,b) (a) < (b) ? (a) : (b)
+#define max(a, b) (a) > (b) ? (a) : (b)
+int *ans;
 
-char * addStrings(char * num1, char * num2){
-    char *p1 = num1 + strlen(num1) - 1;
-    char *p2 = num2 + strlen(num2) - 1;
-    char *ans =( char *)malloc(max(strlen(num1), strlen(num2))*sizeof(char) + 1);
-    char *p3 = ans + strlen(ans) - 1;
-    int n = min(strlen(num1), strlen(num2)) - 1;
-    *p3 = *p1 + *p2;
-    while(n--){
-        *p3 += *p1 + *p2 - '0';
-        if(*p3 > '9'){
-            --*p3;
-            p3--;
-            *p3 = 1;
-        }else{
-            *(--p3) = 0;
-        }
-    }
-    return ans;
+char *addStrings(char *num1, char *num2) {
+  int l1 = strlen(num1);
+  int l2 = strlen(num2);
+  int l = max(l1, l2) + 1;
+  // ans = (char*)malloc(sizeof(char) * (l+1));
+  int c = 0, s;
+  for (l1 = l1 - 1, l2 = l2 - 1, l = l - 1; l1 >= 0 && l2 >= 0;
+       --l1, --l2, --l) {
+    s = num1[l1] + num2[l2] - 2 * '0';
+    c = s / 10;
+    ans[l] = s % 10 + '0';
+  }
+  return ans + c / 10;
 }
-
-
+//
+// 手残,一不小心提交了
+// https://leetcode.com/submissions/detail/243216166/
+// AddressSanitizer: SEGV on unknown address 0x000000000004 (pc 0x000000401934
+// bp 0x7ffd228bf020 sp 0x7ffd228bf000 T0) 因该因为内存溢出
