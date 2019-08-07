016148f05737811a39a32bb799a3cb19d69c2fdf
yichuanshuzi <chenboshuo.hi@163.com>
2019-08-07 18:51:27 +0800

    性能最高的暴力破解法

--- leetcode/0028-Implement-strStr/mycode.c
+++ leetcode/0028-Implement-strStr/mycode.c
@@ -1,26 +1,20 @@
-#include <string.h>
 int strStr(char *haystack, char *needle) {
   if (!*needle) { // 处理两个空字符串的特殊情况
     return 0;
   }
-  int n_len = strlen(needle);
-  int h_len = strlen(haystack);
-  if (n_len > h_len) {
-    return -1;
-  }
-  for (int i = 0; i < h_len; ++i) {
-    int j;
-    for (j = 0; haystack[i + j] == needle[j] && i + j < h_len; ++j) {
+  int j;
+  for (int i = 0; haystack[i]; ++i) {
+    for (j = 0; haystack[i + j] == needle[j] && haystack[i + j]; ++j) {
       ;
     }
-    if (j == n_len) { // needle[j] == '\0' 代表遍历完成
+    if (!needle[j]) { // needle[j] == '\0' 代表遍历完成
       return i;
     }
   }
   return -1;
 }
 
-// https://leetcode.com/submissions/detail/249660399/
-// Runtime: 1356 ms, faster than 11.50% of C online submissions for Implement
-// strStr(). Memory Usage: 7.4 MB, less than 14.29% of C online submissions for
+// https://leetcode.com/submissions/detail/248044156/
+// Runtime: 1300 ms, faster than 13.85% of C online submissions for Implement
+// strStr(). Memory Usage: 7.2 MB, less than 62.89% of C online submissions for
 // Implement strStr().
