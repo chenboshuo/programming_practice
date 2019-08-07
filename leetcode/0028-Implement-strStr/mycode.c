6838c6991091195466ca7fc5c55f9e4fd2c44b15
yichuanshuzi <chenboshuo.hi@163.com>
2019-08-07 18:21:09 +0800

    1304ms(13.52%),7.2MB(61.9%) 试一试提前预知长度,结果速度慢了

--- leetcode/0028-Implement-strStr/mycode.c
+++ leetcode/0028-Implement-strStr/mycode.c
@@ -1,10 +1,16 @@
+#include <string.h>
 int strStr(char *haystack, char *needle) {
   if (!*needle) { // 处理两个空字符串的特殊情况
     return 0;
   }
-  int j;
-  for (int i = 0; haystack[i]; ++i) {
-    for (j = 0; haystack[i + j] == needle[j] && haystack[i + j]; ++j) {
+  int n_len = strlen(needle);
+  int h_len = strlen(haystack);
+  if (n_len > h_len) {
+    return -1;
+  }
+  for (int i = 0; i < h_len; ++i) {
+    int j;
+    for (j = 0; haystack[i + j] == needle[j] && i + j < h_len; ++j) {
       ;
     }
     if (!needle[j]) { // needle[j] == '\0' 代表遍历完成
@@ -14,7 +20,7 @@ int strStr(char *haystack, char *needle) {
   return -1;
 }
 
-// https://leetcode.com/submissions/detail/248044156/
-// Runtime: 1300 ms, faster than 13.85% of C online submissions for Implement
-// strStr(). Memory Usage: 7.2 MB, less than 62.89% of C online submissions for
+// https://leetcode.com/submissions/detail/249660167/
+// Runtime: 1304 ms, faster than 13.52% of C online submissions for Implement
+// strStr(). Memory Usage: 7.2 MB, less than 61.90% of C online submissions for
 // Implement strStr().
