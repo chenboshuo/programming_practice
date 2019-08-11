670c71665d09ab1bf0c45958056e5aec233d3f6b
yichuanshuzi <chenboshuo.hi@163.com>
2019-08-11 18:38:14 +0800

    4ms(79.47%),7.3MB(31.25%) 原来暴力法性能也这么高

--- leetcode/0028-Implement-strStr/README.md
+++ leetcode/0028-Implement-strStr/README.md
@@ -22,3 +22,8 @@ Output: -1
 What should we return when needle is an empty string? This is a great question to ask during an interview.
 
 For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+
+## 参考
+### KMP算法
+-   [Knuth-Morris-Pratt algorithm](http://www.inf.fh-flensburg.de/lang/algorithmen/pattern/kmpen.htm)
+-   [动画：七分钟理解什么是KMP算法](https://mp.weixin.qq.com/s?__biz=MzUyNjQxNjYyMg==&mid=2247485939&idx=1&sn=b25f39b5644da92c4047bbbd9936f73c&chksm=fa0e6672cd79ef64dda0a21e23c2817edf4a64cbb75b9bed328d6519c6cd4fef36d03a4cb309)
--- leetcode/0028-Implement-strStr/mycode.c
+++ leetcode/0028-Implement-strStr/mycode.c
@@ -4,6 +4,7 @@ int strStr(char *haystack, char *needle) {
   }
   for (int i = 0, j = 0; haystack[i]; ++i) {
     if (haystack[i] != needle[j]) {
+      i = i - j;
       j = -1;
     }
     ++j;
@@ -14,10 +15,7 @@ int strStr(char *haystack, char *needle) {
   return -1;
 }
 
-// https://leetcode.com/submissions/detail/250713943/
-// "mississippi"
-// "issip"
-// Output:
-// -1
-// Expected:
-// 4
+// https://leetcode.com/submissions/detail/250714622/
+// Runtime: 4 ms, faster than 79.47% of C online submissions for Implement
+// strStr(). Memory Usage: 7.3 MB, less than 31.25% of C online submissions for
+// Implement strStr().
