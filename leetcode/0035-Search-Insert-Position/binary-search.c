dadeb3f6c0f5fa48fc73f4a7f2b8b0f583617174
yichuanshuzi <chenboshuo.hi@163.com>
2019-08-14 17:30:16 +0800

    4ms(85.85%), 7.1MB(100%) 之前逻辑顺序不对,现在改过来

--- leetcode/0035-Search-Insert-Position/binary-search.c
+++ leetcode/0035-Search-Insert-Position/binary-search.c
@@ -1,32 +1,25 @@
-
-
-int searchInsert(int* nums, int numsSize, int target){
+int searchInsert(int *nums, int numsSize, int target) {
   int left = 0, right = numsSize - 1;
-  if(target > nums[right]){
+  if (target > nums[right]) {
     return numsSize;
   }
-  while(left < right){
-    int mid = (left + right)/2;
-
-    if (target == nums[mid]){
-      return mid;
-    }else if (target > nums[mid]){
-      left = mid;
-    }else{
-      right = mid;
+  while (left < right) {
+    int mid = (left + right) / 2;
+    if (mid == left) {
+      return (target > nums[mid]) ? mid + 1 : mid;
     }
-    if(mid == left){
-      return (target > nums[mid])? mid+1 : mid;
+    if (target == nums[mid]) {
+      return mid;
+    } else if (target > nums[mid]) {
+      left = mid;
+    } else {
+      right = mid;
     }
   }
   return 0;
 }
 
-// https://leetcode.com/submissions/detail/251573361/
-// Input:
-// [1,3,6,9]
-// 9
-// Output:
-// 2
-// Expected:
-// 3
+// https://leetcode.com/submissions/detail/251574416/
+// Runtime: 4 ms, faster than 85.85% of C online submissions for Search
+// Insert Position. Memory Usage: 7.1 MB, less than 100.00% of C online
+// submissions for Search Insert Position.
