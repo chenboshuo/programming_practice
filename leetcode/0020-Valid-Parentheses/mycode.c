3fa94cc96f6b609ff95de883dea6bb4aa3a15802
yichuanshuzi <chenboshuo.hi@163.com>
2019-10-10 19:09:43 +0800

    :art: Beautify the code. 美化代码

--- leetcode/0020-Valid-Parentheses/mycode.c
+++ leetcode/0020-Valid-Parentheses/mycode.c
@@ -1,21 +1,52 @@
+#include <stdbool.h>  // 定义bool变量
 #include <string.h>
+
 bool isValid(char* s) {
-    int brackets[strlen(s)+2];
-    int *p = brackets+1;
-    for(int i=0; *(s+i); i++){
-        switch(*(s+i)){
-                case'(': *p=1; p++; break;
-                case')': if(*(p-1) == 1) {p--;} else{return false;}break;
-                case'[': *p=2; p++; break;
-                case']': if(*(p-1) == 2) {p--;} else{return false;} break;
-                case'{': *p = 3; p++; break;
-                case'}': if(*(p-1) == 3) {p--;} else{return false;} break;
+  int brackets[strlen(s) + 2];
+  int* p = brackets + 1;
+  for (int i = 0; s[i]; i++) {
+    switch (s[i]) {
+      case '(':
+        *p = 1;
+        p++;
+        break;
+      case ')':
+        if (*(p - 1) == 1) {
+          p--;
+        } else {
+          return false;
         }
+        break;
+      case '[':
+        *p = 2;
+        p++;
+        break;
+      case ']':
+        if (*(p - 1) == 2) {
+          p--;
+        } else {
+          return false;
+        }
+        break;
+      case '{':
+        *p = 3;
+        p++;
+        break;
+      case '}':
+        if (*(p - 1) == 3) {
+          p--;
+        } else {
+          return false;
+        }
+        break;
     }
-    if(p != brackets+1){
-        return false;
-    }
-    return true;
+  }
+  if (p != brackets + 1) {
+    return false;
+  }
+  return true;
 }
-// Runtime: 4 ms, faster than 69.72% of C online submissions for Valid Parentheses.
-// Memory Usage: 7.3 MB, less than 64.10% of C online submissions for Valid Parentheses.
+// Runtime: 4 ms, faster than 69.72% of C online submissions for Valid
+// Parentheses.
+// Memory Usage: 7.3 MB, less than 64.10% of C online submissions
+// for Valid Parentheses.
