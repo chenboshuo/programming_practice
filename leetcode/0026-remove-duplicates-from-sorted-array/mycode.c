0f1154a3d8ea188bd5fc3de75c22cecda01ef56a
yichuanshuzi <chenboshuo.hi@163.com>
2019-12-05 17:11:59 +0800

    :tada: 16ms(92.42%), 9.6MB(50%)

--- leetcode/0026-remove-duplicates-from-sorted-array/mycode.c
+++ leetcode/0026-remove-duplicates-from-sorted-array/mycode.c
@@ -1,14 +1,25 @@
+/**
+ * 去重
+ * @param  nums     数组
+ * @param  numsSize 数组大小
+ * @return          新的数组的大小
+ */
 int removeDuplicates(int* nums, int numsSize){
-  int *pre = nums, *q = nums;
-  for(int i = 1; i < numsSize; i++){
-    if (*pre == *q) {
-      ++q;
-    }
-    else {
-      ++q;
-      *pre = *q;
-      ++pre;
+  if(numsSize<2){
+    return numsSize;
+  }
+
+  int pre;
+  int post = 0;
+
+
+  for(pre = 1;pre < numsSize;++pre){
+    if(nums[post] != nums[pre]){
+      nums[++post] = nums[pre];
     }
   }
-  return pre - nums + 1;
+  return post+1;
 }
+
+// Runtime: 16 ms, faster than 92.42% of C online submissions for Remove Duplicates from Sorted Array.
+// Memory Usage: 9.6 MB, less than 50.00% of C online submissions for Remove Duplicates from Sorted Array.
