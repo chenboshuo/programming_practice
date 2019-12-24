fb8aaad27b12b9d4b757425e2443f54feffb24eb
yichuanshuzi <chenboshuo.hi@163.com>
2019-12-24 19:38:09 +0800

    :zap: 782ms(69.19%), 0B

--- leetcode/0175-Combine-Two-Tables/myquery.sql
+++ leetcode/0175-Combine-Two-Tables/myquery.sql
@@ -1,7 +1,6 @@
 /* Write your T-SQL query statement below */
 select FirstName, LastName, City, State
 from Address
-  right join Person on Address.PersonId = Person.PersonId
-
-  -- Runtime: 935 ms, faster than 50.09% of MS SQL Server online submissions for Combine Two Tables.
+  right outer join Person on Address.PersonId = Person.PersonId
+  -- Runtime: 782 ms, faster than 69.19% of MS SQL Server online submissions for Combine Two Tables.
   -- Memory Usage: 0B, less than 100.00% of MS SQL Server online submissions for Combine Two Tables.
