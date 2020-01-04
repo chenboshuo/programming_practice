93b70f88b9d0ecc29dde830f6f16325e69537d5f
yichuanshuzi <chenboshuo.hi@163.com>
2020-01-04 11:08:30 +0800

    :zap: 36ms(75%), 9.8MB(100%)

--- leetcode/1306.-Jump-Game-III/recurrence.c
+++ leetcode/1306.-Jump-Game-III/recurrence.c
@@ -1,18 +1,18 @@
+// reference: https://leetcode.com/problems/jump-game-iii/discuss/464083/C%2B%2BJava-3-lines-recursion
 #include <stdbool.h>
 
 bool canReach(int* arr, int arrSize, int start){
-  if(arr[start] == 0){
-    return true;
-  }
-  if(arr[start] == -1){
+
+  if(start<0 || start>arrSize-1 || arr[start]==-1){
     return false;
   }
-  int to = arr[start];
+  int delta = arr[start];
   arr[start] = -1;
 
-  return  ((start+to < arrSize)  && canReach(arr, arrSize, start+to) )
-          ||( (start-to > -1) && canReach(arr, arrSize, start-to)); // 递归-1 参与运算
+  return  (delta == 0)
+        || canReach(arr, arrSize, start+delta)
+        || canReach(arr, arrSize, start-delta);
 }
 
-// Runtime: 44 ms
-// Memory Usage: 10 MB
+// Runtime: 36 ms, faster than 75.00% of C online submissions for Jump Game III.
+// Memory Usage: 9.8 MB, less than 100.00% of C online submissions for Jump Game III.
