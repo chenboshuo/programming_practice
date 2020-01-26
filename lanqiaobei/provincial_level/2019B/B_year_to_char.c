d025ea79caeb575f9b7b976a25a58d7ea8109286
chenboshuo <chenboshuo.hi@163.com>
2020-01-26 18:34:52 +0800

    :sparkles: Add some solution of problems. 增加一些题解

---
+++ lanqiaobei/provincial_level/2019B/B_year_to_char.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+// 10 进制转类 26 进制 (AA) = 1*26 + 1
+int main(int argc, char *argv[]){
+  int a = 2019;
+  while(a){
+    printf("%c", a%26 + 'A' -1);
+    a /= 26;
+  }
+  printf("\n");
+}
