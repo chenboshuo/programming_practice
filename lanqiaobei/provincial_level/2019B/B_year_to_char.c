6b416b79a4b13281ab6ec5cabe296bebdb00d33f
chenboshuo <chenboshuo.hi@163.com>
2020-01-26 20:21:32 +0800

    :art: use recursion to print reverse. 用递归倒序打印字符串

--- lanqiaobei/provincial_level/2019B/B_year_to_char.c
+++ lanqiaobei/provincial_level/2019B/B_year_to_char.c
@@ -2,14 +2,30 @@
 #include <stdio.h>
 
 // 10 进制转类 26 进制 (AA) = 1*26 + 1
-int main(int argc, char *argv[]){
-  int a = 2019;
-  while(a){
-    printf("%c", a%26 + 'A' -1);
-    a /= 26;
-  }
-  printf("\n");
-}
+// int main(int argc, char *argv[]){
+//   int a = 2019;
+//   while(a){
+//     printf("%c", a%26 + 'A' -1);
+//     a /= 26;
+//   }
+//   printf("\n");
+// }
 
 // QYB
 // 注意倒序
+
+void year_to_char(int a){
+  if (a == 0) return;
+  year_to_char(a/26);
+  printf("%c", a%26 + 'A' -1);
+}
+
+int main(int argc, char *argv[]){
+  year_to_char(2019);
+  printf("\n");
+}
+
+
+// BYQ
+
+
