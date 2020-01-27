6d19aa6c840b254eeb4c03d9ce413e2f27c28763
chenboshuo <chenboshuo.hi@163.com>
2020-01-27 15:41:04 +0800

    :beer: still wrong. 错误尝试

--- leetcode/0415-add-strings/mycode.py
+++ leetcode/0415-add-strings/mycode.py
@@ -3,15 +3,19 @@ class Solution:
         ans = ""
         c = 0
         for i,j in zip(num1[::-1], num2[::-1]):
-          ans += str((ord(i) + ord(j) + c - ord('0')*2) % 10)
-          c = (ord(i)+ord(j)) // 10
+          s = ord(i) + ord(j) - 2*ord('0')
+          ans += str(s % 10)
+          c = s // 10
+        if c:
+          ans += str(c)
         return ans[::-1]
 
+
 # Input
-# "1"
 # "9"
+# "99"
 # Output
-# "0"
+# "18"
 # Expected
-# "10"
-# https://leetcode.com/submissions/detail/297808272/
+# "108"
+# https://leetcode.com/submissions/detail/297809278/
