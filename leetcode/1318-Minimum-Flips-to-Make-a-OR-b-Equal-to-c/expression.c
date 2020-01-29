7b7757a0f87df30baeedd4654f1b1f72b7a03840
chenboshuo <chenboshuo.hi@163.com>
2020-01-29 18:09:29 +0800

    :sparkles: 0ms(100%), 6.5MB(100%)

--- leetcode/1318-Minimum-Flips-to-Make-a-OR-b-Equal-to-c/expression.c
+++ leetcode/1318-Minimum-Flips-to-Make-a-OR-b-Equal-to-c/expression.c
@@ -1,17 +1,20 @@
-#include <math.h>
 int minFlips(int a, int b, int c){
-  int ai,bi,ci,count=0;
+  int ai,bi,ci,count=0,_;
   while(a||b||c){
     ai = a&1;
     bi = b&1;
     ci = c&1;
-    count += ((ai|bi)^ci)*fabs(ai+bi-ci);
+    if((_=ai+bi-ci) < 0){
+      _ *= -1;
+    }
+    count += ((ai|bi)^ci)*(_) ;
     c>>=1;
     b>>=1;
     a>>=1;
   }
   return count;
 }
-// Runtime: 4 ms, faster than 45.83% of C online submissions for Minimum Flips to Make a OR b Equal to c.
-// Memory Usage: 6.8 MB, less than 100.00% of C online submissions for Minimum Flips to Make a OR b Equal to c.
-// https://leetcode.com/submissions/detail/298406966/
+
+// Runtime: 0 ms, faster than 100.00% of C online submissions for Minimum Flips to Make a OR b Equal to c.
+// Memory Usage: 6.5 MB, less than 100.00% of C online submissions for Minimum Flips to Make a OR b Equal to c.
+// https://leetcode.com/submissions/detail/298407923/
