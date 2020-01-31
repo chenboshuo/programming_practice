d2ba824d808d80266d6a54b74154d4dae45eaf5e
chenboshuo <chenboshuo.hi@163.com>
2020-01-31 17:03:15 +0800

    :hankey: Another try. 又一次尝试

--- leetcode/1296-Divide-Array-in-Sets-of-K-Consecutive-Numbers/mycode.c
+++ leetcode/1296-Divide-Array-in-Sets-of-K-Consecutive-Numbers/mycode.c
@@ -5,32 +5,61 @@ int comp(const void* a, const void*b){
   return *(int*)a - *(int*)b;
 }
 
+
 bool isPossibleDivide(int* nums, int numsSize, int k){
   if(numsSize % k != 0){
     return false;
   }
+
   qsort(nums, numsSize, sizeof(int), comp);
-  int n_group = numsSize/k;
-  int *groups = (int*)malloc(sizeof(int)* n_group);
-  int begin = 0;
-  int active = 1;
+  int len_sets = numsSize / k;   // 每组最大长度
+  int *len = (int*)calloc(len_sets, sizeof(int)); // 每一组的长度
+  // int final[numsSize/k];
+  int *final = (int*)calloc(len_sets, sizeof(int)); // 每一组的最后一个数
+  int begin = 0; //
+  int end = 0;
+  int i;
+  int pre = -1; // 记录前一个数
 
-  while(begin<n_group){
-    for(int i=0; i<active;++i){
-      
+  while(numsSize--){
+    if(*nums != pre && *nums != pre+1){
+      i = begin;
     }
-  }
-  free(groups);
-  return true;
 
+    for(;i>begin-1 &&i<end; ++i){
+      if (*nums == final[i] + 1){
+        final[i] = *nums;
+        if(++len[i] == k){ // 一组存满, 表示开始的指针后移
+          ++begin;
+        }
+        break;
+      }
+    }
+    if(i == end){
+      if(++end > len_sets){
+        return false;
+      }else{
+        final[i] = *nums;
+        ++len[i];
+      }
+
+    }
+
+    pre = *nums;
+    ++nums;
+  }
+
+  return true;
 }
 
-// https://leetcode.com/submissions/detail/289094225/
 // Wrong Answer
+// Details
 // Input
-// [12,12,2,11,22,20,11,13,3,21,1,13]
-// 3
+// [1,2,3,3,4,4,5,6]
+// 4
 // Output
 // false
 // Expected
 // true
+//
+// https://leetcode.com/submissions/detail/298980137/
