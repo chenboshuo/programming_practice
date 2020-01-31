40efd110c44ecf31be54653065b270a8a13c748a
chenboshuo <chenboshuo.hi@163.com>
2020-01-31 17:25:52 +0800

    :sparkles: 620ms(22.2%), 13.4MB(100%).

--- leetcode/1296-Divide-Array-in-Sets-of-K-Consecutive-Numbers/mycode.c
+++ leetcode/1296-Divide-Array-in-Sets-of-K-Consecutive-Numbers/mycode.c
@@ -19,7 +19,7 @@ bool isPossibleDivide(int* nums, int numsSize, int k){
   int begin = 0; //
   int end = 0;
   int i;
-  int pre = -1; // 记录前一个数
+  // int pre = -1; // 记录前一个数
 
   while(numsSize--){
     // 应该不能偷懒
@@ -31,6 +31,9 @@ bool isPossibleDivide(int* nums, int numsSize, int k){
       if (*nums == final[i] + 1){
         final[i] = *nums;
         if(++len[i] == k){ // 一组存满, 表示开始的指针后移
+          if(i>1 && len[i-1] < len[i]){ // 前一组没有存满, 那一组永远满不了
+            return false;
+          }
           ++begin;
         }
         break;
@@ -46,18 +49,14 @@ bool isPossibleDivide(int* nums, int numsSize, int k){
 
     }
 
-    pre = *nums;
+    // pre = *nums;
     ++nums;
   }
 
   return true;
 }
-// Input
-// [16,5,15,15,20,16,20,14,21,20,19,20,12,17,13,15,11,17,18,18,11,13,13,14,14,9,20,18,10,4,4,6,15,19,8,15,7,17,15,9,24,2,23,22,26,8,21,22,14,13,16,2,25,23,17,19,17,3,22,23,19,12,21,12,16,27,28,10,13,8,24,3,22,6,10,9,14,7,11,22,11,5,16,19,21,2,8,24,16,21,7,29,18,9,10,18,6,17,21,20]
-// 10
-// Output
-// true
-// Expected
-// false
-//
-// https://leetcode.com/submissions/detail/298981067/
+// Runtime: 620 ms, faster than 22.22% of C
+// online submissions for Divide Array in Sets of K Consecutive Numbers.
+// Memory Usage: 13.4 MB, less than 100.00% of C
+// online submissions for Divide Array in Sets of K Consecutive Numbers.
+// https://leetcode.com/submissions/detail/298983439/
