e7fc85d2fec3326e4893b505d976e93ea3fd60dd
chenboshuo <chenboshuo.hi@163.com>
2020-02-09 17:35:50 +0800

    :zap: 604ms(92.22%), 34.4MB(100%)

--- leetcode/1338-Reduce-Array-Size-to-The-Half/mycode.py
+++ leetcode/1338-Reduce-Array-Size-to-The-Half/mycode.py
@@ -4,12 +4,11 @@ class Solution:
       a = Counter(arr)
       half = len(arr)//2
       total = 0
-      for index,i in enumerate(sorted(Counter(arr).values(), reverse=True)):
-        total += i
+      for index,i in enumerate(a.most_common()):
+        total += i[1]
         if total >= half:
           return index+1;
-        
-# https://leetcode.com/submissions/detail/301640520/
-# Runtime: 832 ms, faster than 26.18% of Python3 online submissions
-# Memory Usage: 33.2 MB, less than 100.00% of Python3 online submissions.
-# reference https://leetcode.com/problems/reduce-array-size-to-the-half/discuss/496703/Python-Simple-Greedy-Solution-using-Counter-and-Sorting
+          
+# https://leetcode.com/submissions/detail/301649219/
+# Runtime: 604 ms, faster than 92.22% of Python3 online submissions for Reduce Array Size to The Half.
+# Memory Usage: 34.4 MB, less than 100.00% of Python3 online submissions for Reduce Array Size to The Half.
