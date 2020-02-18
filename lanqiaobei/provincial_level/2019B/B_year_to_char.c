466da9af65e392f68bd67ba0bca3f4b59a0c7076
chenboshuo <chenboshuo.hi@163.com>
2020-02-18 16:09:41 +0800

    :sparkles: Fix the bugs about 'Z'. 修复关于'Z'的bug

--- lanqiaobei/provincial_level/2019B/B_year_to_char.c
+++ lanqiaobei/provincial_level/2019B/B_year_to_char.c
@@ -1,5 +1,5 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 // 10 进制转类 26 进制 (AA) = 1*26 + 1
 // int main(int argc, char *argv[]){
@@ -14,18 +14,21 @@
 // QYB
 // 注意倒序
 
-void year_to_char(int a){
+void year_to_char(int a) {
   if (a == 0) return;
-  year_to_char(a/26);
-  printf("%c", a%26 + 'A' -1);
+  year_to_char((a - 1) / 26);
+  printf("%c", (a - 1) % 26 + 'A');
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
   year_to_char(2019);
+  printf("\n");
+  year_to_char(26);
+  printf("\n");
+  year_to_char(52);
+  printf("\n");
+  year_to_char(27);
   printf("\n");
 }
 
-
 // BYQ
-
-
