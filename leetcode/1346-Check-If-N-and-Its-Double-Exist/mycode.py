f87f76392615631a517e3d495863284cb5876af1
chenboshuo <chenboshuo.hi@163.com>
2020-02-21 17:21:19 +0800

    :sparkles: 44ms(96.34%),12.9MB

--- leetcode/1346-Check-If-N-and-Its-Double-Exist/mycode.py
+++ leetcode/1346-Check-If-N-and-Its-Double-Exist/mycode.py
@@ -1,14 +1,11 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
+      if arr.count(0) > 1:
+        return True
+
       a = set(arr)
       double_a = set([i*2 for i in arr])
       if (double_a & a) - set([0]):
         return True
       else:
         return False
-# Input:
-# [0,0]
-# Output:
-# false
-# Expected:
-# true
