db2787ff682c8d4d94fcd1cf909bb79054b2fe37
chenboshuo <chenboshuo.hi@163.com>
2020-02-22 16:45:41 +0800

    :zap: 4ms(92.13%), 7.2MB(100%)

--- leetcode/1346-Check-If-N-and-Its-Double-Exist/hash.c
+++ leetcode/1346-Check-If-N-and-Its-Double-Exist/hash.c
@@ -2,14 +2,15 @@
 bool checkIfExist(int* arr, int arrSize) {
   bool exist[4000 + 1] = {false};
   for (int i = 0; i < arrSize; ++i) {
-    if (exist[2000 + arr[i] * 2] ||
-        (!(arr[i] & 1) && exist[2000 + arr[i] / 2])) {
+    int num = arr[i];
+    if (exist[2000 + num * 2] || (!(num & 1) && exist[2000 + num / 2])) {
       return true;
     }
-    exist[2000 + arr[i]] = true;
+    exist[2000 + num] = true;
   }
   return false;
 }
-// Runtime: 8 ms, faster than 58.43% of C online submissions for Check If N and
-// Its Double Exist. Memory Usage: 7.3 MB, less than 100.00% of C online
+
+// Runtime: 4 ms, faster than 92.13% of C online submissions for Check If N and
+// Its Double Exist. Memory Usage: 7.2 MB, less than 100.00% of C online
 // submissions for Check If N and Its Double Exist.
