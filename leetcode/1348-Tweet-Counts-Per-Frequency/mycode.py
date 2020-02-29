79d4cbed123c8583a21f5bbee5eb71f3eb24de96
chenboshuo <chenboshuo.hi@163.com>
2020-02-29 15:34:24 +0800

    :zap: 412ms(55.39%), 20.5MB(100%)

--- leetcode/1348-Tweet-Counts-Per-Frequency/mycode.py
+++ leetcode/1348-Tweet-Counts-Per-Frequency/mycode.py
@@ -10,18 +10,16 @@ class TweetCounts:
 
 
     def getTweetCountsPerFrequency(self, freq: str, tweetName: str, startTime: int, endTime: int) -> List[int]:
-      # 处理分钟情况
-        time_during = {'minute': 60, 'hour':3600, 'day': 86400}
-        delta = time_during[freq]
+        delta = 60 if freq == 'minute' else 3600 if freq == 'hour' else 86400
         n_duration = (endTime - startTime) // delta + 1
         ans = [0]* n_duration
 
         for time in self.user_time[tweetName]:
           if startTime<= time <= endTime:
             ans[(time-startTime) // delta]+=1
-
-        return ans
         
+        return ans
+
 
 
 # Your TweetCounts object will be instantiated and called as such:
@@ -29,5 +27,5 @@ class TweetCounts:
 # obj.recordTweet(tweetName,time)
 # param_2 = obj.getTweetCountsPerFrequency(freq,tweetName,startTime,endTime)
 
-# Runtime: 420 ms, faster than 51.70% of Python3 online submissions for Tweet Counts Per Frequency.
-# Memory Usage: 20.4 MB, less than 100.00% of Python3 online submissions for Tweet Counts Per Frequency.
+# Runtime: 412 ms, faster than 55.39% of Python3 online submissions for Tweet Counts Per Frequency.
+# Memory Usage: 20.5 MB, less than 100.00% of Python3 online submissions for Tweet Counts Per Frequency.
