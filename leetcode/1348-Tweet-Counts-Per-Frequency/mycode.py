1086512e67fc464a408fe548323de9f2a199c04b
chenboshuo <chenboshuo.hi@163.com>
2020-02-29 15:31:50 +0800

    :zap: 420ms(51.7%), 20.4MB(100%)

--- leetcode/1348-Tweet-Counts-Per-Frequency/mycode.py
+++ leetcode/1348-Tweet-Counts-Per-Frequency/mycode.py
@@ -1,13 +1,12 @@
 from collections import defaultdict
+from bisect import insort
 class TweetCounts:
 
     def __init__(self):
         self.user_time = defaultdict(list)
 
-    def recordTweet(self, tweetName: str, time: int) -> None:
-      self.user_time[tweetName].append(time)
-
-      self.user_time[tweetName].sort()
+    def recordTweet(self, tweet_name: str, time: int) -> None:
+      insort(self.user_time[tweet_name], time)
 
 
     def getTweetCountsPerFrequency(self, freq: str, tweetName: str, startTime: int, endTime: int) -> List[int]:
@@ -20,9 +19,9 @@ class TweetCounts:
         for time in self.user_time[tweetName]:
           if startTime<= time <= endTime:
             ans[(time-startTime) // delta]+=1
-        
-        return ans
 
+        return ans
+        
 
 
 # Your TweetCounts object will be instantiated and called as such:
@@ -30,6 +29,5 @@ class TweetCounts:
 # obj.recordTweet(tweetName,time)
 # param_2 = obj.getTweetCountsPerFrequency(freq,tweetName,startTime,endTime)
 
-
-# Runtime: 444 ms, faster than 40.45% of Python3 online submissions for Tweet Counts Per Frequency.
-# Memory Usage: 20.3 MB, less than 100.00% of Python3 online submissions for Tweet Counts Per Frequency.
+# Runtime: 420 ms, faster than 51.70% of Python3 online submissions for Tweet Counts Per Frequency.
+# Memory Usage: 20.4 MB, less than 100.00% of Python3 online submissions for Tweet Counts Per Frequency.
