96d1e44a5ada8ccbdb8023451a2b194384fac6d7
chenboshuo <chenboshuo.hi@163.com>
2020-03-24 09:50:20 +0800

    :hankey: index out of range

--- leetcode/1352_Product_of_the_Last_K_Numbers/mycode.py
+++ leetcode/1352_Product_of_the_Last_K_Numbers/mycode.py
@@ -5,16 +5,14 @@ class ProductOfNumbers:
 
     def __init__(self):
         self.nums = []
-        self.left_product = 1
         self.product = 1
 
     def add(self, num: int) -> None:
         if num != 0:
             # for i in range(len(self.nums)):
             #   self.nums[i] *= num
+            self.nums.append(self.product)
             self.product *= num
-            num *= self.left_product
-            self.nums.append(num)
         else:
             self.nums = [0 for _ in self.nums]
 
@@ -37,15 +35,12 @@ class ProductOfNumbers:
 # obj.add(num)
 # param_2 = obj.getProduct(k)
 
-
-# 1 / 33 test cases passed.
-# Status: Wrong Answer
+# 2 / 33 test cases passed.
+# Status: Runtime Error
 # Submitted: 1 month ago
-# Input:
-# ["ProductOfNumbers","add","add","add","add","add","getProduct","getProduct","getProduct","add","getProduct"]
-# [[],[3],[0],[2],[5],[4],[2],[3],[4],[8],[2]]
-# Output:
-# [null,null,null,null,null,null,24,60,0,null,240]
-# Expected:
-# [null,null,null,null,null,null,20,40,0,null,32]
-# https://leetcode.com/contest/weekly-contest-176/submissions/detail/303678592/
+# Runtime Error Message:
+# Line 24: IndexError: list index out of range
+# Last executed input:
+# ["ProductOfNumbers","add","add","add","add","add","getProduct","add","getProduct","add"]
+# [[],[3],[0],[0],[8],[3],[1],[7],[5],[3]]
+# https://leetcode.com/contest/weekly-contest-176/submissions/detail/303683901/
