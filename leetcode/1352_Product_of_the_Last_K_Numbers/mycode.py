83a50b10f28ececc51797789cf7347d76f013f90
chenboshuo <chenboshuo.hi@163.com>
2020-03-24 09:56:31 +0800

    :zap: 276ms(96.18%), 28.1MB(100%), using prefix product

--- leetcode/1352_Product_of_the_Last_K_Numbers/mycode.py
+++ leetcode/1352_Product_of_the_Last_K_Numbers/mycode.py
@@ -1,35 +1,20 @@
-# import numpy as np
-
-
+# reference https://leetcode.com/problems/product-of-the-last-k-numbers/discuss/510260/JavaC%2B%2BPython-Prefix-Product
 class ProductOfNumbers:
 
     def __init__(self):
-        self.nums = []
-        self.product = 1
+        self.nums = [1]
 
     def add(self, num: int) -> None:
         if num != 0:
-            # for i in range(len(self.nums)):
-            #   self.nums[i] *= num
-            self.nums.append(self.product)
-            self.product *= num
+            self.nums.append(self.nums[-1]*num)
         else:
-            self.nums = [0 for _ in self.nums]
-            self.nums.append(0)
-            self.product = 1
+            self.nums = [1]
 
     def getProduct(self, k: int) -> int:
-        # return self.nums[-k]
-        # ans = 1
-        # for i in self.nums[-k:]:
-        #   ans *= i
-        # return ans
-        # return np.product(self.nums[-k:])
-        p = self.nums[-k]
-        if p == 0:
+        if(k >= len(self.nums)):
             return 0
         else:
-            return self.product//p
+            return self.nums[-1] // self.nums[-k-1]
 
 
 # Your ProductOfNumbers object will be instantiated and called as such:
@@ -37,9 +22,6 @@ class ProductOfNumbers:
 # obj.add(num)
 # param_2 = obj.getProduct(k)
 
-# 33 / 33 test cases passed.
-# Status: Accepted
-# Runtime: 1920 ms (10.8%)
-# Memory Usage: 43.5 MB
-# Submitted: 1 month ago
-# https://leetcode.com/contest/weekly-contest-176/submissions/detail/303692689/
+# Runtime: 276 ms, faster than 96.18% of Python3 online submissions for Product of the Last K Numbers.
+# Memory Usage: 28.1 MB, less than 100.00% of Python3 online submissions for Product of the Last K Numbers.
+# https://leetcode.com/submissions/detail/315360973/
