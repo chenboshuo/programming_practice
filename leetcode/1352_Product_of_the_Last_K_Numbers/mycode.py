71c9f36eb76ecd1930f350b7bdc91724fc7a7f49
chenboshuo <chenboshuo.hi@163.com>
2020-03-24 09:49:03 +0800

    :hankey: wrong answer

--- leetcode/1352_Product_of_the_Last_K_Numbers/mycode.py
+++ leetcode/1352_Product_of_the_Last_K_Numbers/mycode.py
@@ -5,12 +5,18 @@ class ProductOfNumbers:
 
     def __init__(self):
         self.nums = []
+        self.left_product = 1
+        self.product = 1
 
     def add(self, num: int) -> None:
-        # if num != 0:
-        # for i in range(len(self.nums)):
-        #   self.nums[i] *= num
-        self.nums.append(num)
+        if num != 0:
+            # for i in range(len(self.nums)):
+            #   self.nums[i] *= num
+            self.product *= num
+            num *= self.left_product
+            self.nums.append(num)
+        else:
+            self.nums = [0 for _ in self.nums]
 
     def getProduct(self, k: int) -> int:
         # return self.nums[-k]
@@ -18,7 +24,13 @@ class ProductOfNumbers:
         # for i in self.nums[-k:]:
         #   ans *= i
         # return ans
-        return np.product(self.nums[-k:])
+        # return np.product(self.nums[-k:])
+        p = self.nums[-k]
+        if p == 0:
+            return 0
+        else:
+            return self.product//p
+
 
 # Your ProductOfNumbers object will be instantiated and called as such:
 # obj = ProductOfNumbers()
@@ -26,6 +38,14 @@ class ProductOfNumbers:
 # param_2 = obj.getProduct(k)
 
 
-# 30 / 33 test cases passed.
-# Status: Time Limit Exceeded
-# https://leetcode.com/contest/weekly-contest-176/submissions/detail/303667250/
+# 1 / 33 test cases passed.
+# Status: Wrong Answer
+# Submitted: 1 month ago
+# Input:
+# ["ProductOfNumbers","add","add","add","add","add","getProduct","getProduct","getProduct","add","getProduct"]
+# [[],[3],[0],[2],[5],[4],[2],[3],[4],[8],[2]]
+# Output:
+# [null,null,null,null,null,null,24,60,0,null,240]
+# Expected:
+# [null,null,null,null,null,null,20,40,0,null,32]
+# https://leetcode.com/contest/weekly-contest-176/submissions/detail/303678592/
