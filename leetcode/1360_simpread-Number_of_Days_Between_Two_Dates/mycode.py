c041e3e85f63bdf7f3b4feef1923f163a6d761b8
chenboshuo <chenboshuo.hi@163.com>
2020-04-02 15:18:35 +0800

    :sparkles: 24ms(88.73%), 12.7MB

--- leetcode/1360_simpread-Number_of_Days_Between_Two_Dates/mycode.py
+++ leetcode/1360_simpread-Number_of_Days_Between_Two_Dates/mycode.py
@@ -3,16 +3,11 @@ from datetime import date
 
 class Solution:
     def daysBetweenDates(self, date1: str, date2: str) -> int:
-        return (date.fromisoformat(date2) - date.fromisoformat(date1)).days
+        return abs((date.fromisoformat(date2) - date.fromisoformat(date1)).days)
 
 
-# 56 / 105 test cases passed.
-# Status: Wrong Answer
+# 105 / 105 test cases passed.
+# Status: Accepted
+# Runtime: 24 ms
+# Memory Usage: 12.7 MB
 # Submitted: 1 month, 1 week ago
-# Input:
-# "2020-01-15"
-# "2019-12-31"
-# Output:
-# -15
-# Expected:
-# 15
