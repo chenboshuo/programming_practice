d75069680f11b28b4b761346bd8007d4e92638a6
chenboshuo <chenboshuo.hi@163.com>
2020-04-03 11:23:56 +0800

    :sparkles: 8ms, 5.2MB

--- leetcode/0202_Happy_Number/code.c
+++ leetcode/0202_Happy_Number/code.c
@@ -1,38 +1,44 @@
 #include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
-int happy[10000];
+#define MEMORY_SIZE 10000
+int happy[MEMORY_SIZE];
 
 bool _isHappy(int n) {
   if (n == 1) {
     return true;
   }
-  if (happy[n] != -1) {
+  if (n < MEMORY_SIZE && happy[n] != -1) {
     return happy[n];
   }
 
-  happy[n] = 0;
   int sum_squares = 0;
   int digits = n;
   while (digits) {
     sum_squares += pow(digits % 10, 2);
     digits /= 10;
   }
-  happy[n] = _isHappy(sum_squares);
-  return happy[n];
+
+  if (n < MEMORY_SIZE) {
+    happy[n] = 0;
+    happy[n] = _isHappy(sum_squares);
+    return happy[n];
+  } else {
+    return _isHappy(sum_squares);
+  }
 }
 
 bool isHappy(int n) {
-  for (int i = 0; i < 10000; ++i) {
+  for (int i = 0; i < MEMORY_SIZE; ++i) {
     happy[i] = -1;
   }
   return _isHappy(n);
 }
 
-// https://leetcode.com/submissions/detail/318571714/
-// 350 / 401 test cases passed.
-// Status: Runtime Error
-// Submitted: 17 hours, 12 minutes ago
-// Runtime Error Message:
-// Line 10: Char 12: runtime error: index 1111111 out of bounds for type 'int
-// [10000]' (solution.c) Last executed input: 1111111
+// https://leetcode.com/submissions/detail/318573140/
+
+// 401 / 401 test cases passed.
+// Status: Accepted
+// Runtime: 8 ms
+// Memory Usage: 5.2 MB
+// Submitted: 17 hours, 13 minutes ago
