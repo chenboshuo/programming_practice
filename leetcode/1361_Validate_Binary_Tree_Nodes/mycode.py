0f5b82d22d4b971ea9ae14167de2d3f86f8fbecd
chenboshuo <chenboshuo.hi@163.com>
2020-04-03 16:41:28 +0800

    :art: pared down code. 精简代码(但是之前有个错误)

--- leetcode/1361_Validate_Binary_Tree_Nodes/mycode.py
+++ leetcode/1361_Validate_Binary_Tree_Nodes/mycode.py
@@ -1,25 +1,32 @@
+NULL = -1
+INIT = -2
+
+
 class Solution:
     def validateBinaryTreeNodes(self, n: int, leftChild: List[int], rightChild: List[int]) -> bool:
-        parents = [-2]*n
-        parents[0] = -1  # 根节点无parent,被赋值说明被替换
-        for i, c in enumerate(zip(leftChild, rightChild)):
-            l, r = c
-            if l > -1:
-                if parents[l] != -2:  # parent 被替换
-                    return False
-                parents[l] = i
-            if r > -1:
-                if parents[r] != -2:  # parent 被替换
-                    return False
-                parents[r] = i
-        if -2 in parents:
+        parents = [INIT]*n  # INIT 代表还没有确定parent, 否则为对应索引
+        parents[0] = NULL  # 根节点无parent,被赋值说明被替换
+        for i, children in enumerate(zip(leftChild, rightChild)):
+            for child in children:
+                if child != NULL:
+                    if parents[child] != INIT:  # parent 被替换
+                        return False
+                    # 标记parent
+                    parents[child] = i
+        if INIT in parents:
             return False
 
         return True
 
 
-# 30 / 30 test cases passed.
-# Status: Accepted
-# Runtime: 296 ms
-# Memory Usage: 14.3 MB
-# Submitted: 1 month, 1 week ago
+# Wrong Answer
+# Details
+# Playground Debug
+# Input
+# 2
+# [-1, 0]
+# [-1, -1]
+# Output
+# false
+# Expected
+# true
