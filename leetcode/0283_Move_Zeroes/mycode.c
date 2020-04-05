eb3acc6a4fb5a7e602888313ebc6aceaa9f0bf3f
chenboshuo <chenboshuo.hi@163.com>
2020-04-05 09:46:51 +0800

    :zap: reduce the total number of operations. 减少交换操作次数

--- leetcode/0283_Move_Zeroes/mycode.c
+++ leetcode/0283_Move_Zeroes/mycode.c
@@ -1,21 +1,19 @@
 
-
 void moveZeroes(int* nums, int numsSize) {
   int slow = 0;
   for (int fast = 0; fast < numsSize; ++fast) {
     if (nums[fast] != 0) {
-      // exchange
-      int temp = nums[fast];
-      nums[fast] = nums[slow];
-      nums[slow] = temp;
-      ++slow;
+      nums[slow] = nums[fast];
+      slow++;
     }
+  }
+  while (slow < numsSize) {
+    nums[slow++] = 0;
   }
 }
 
-// Submission Detail
 // 21 / 21 test cases passed.
 // Status: Accepted
 // Runtime: 12 ms
-// Memory Usage: 7.2 MB
-// Submitted: 15 hours, 20 minutes ago
+// Memory Usage: 7.5 MB
+// Submitted: 4 minutes ago
