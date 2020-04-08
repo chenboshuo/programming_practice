5ad62c101560d6b2e471d4366752d6262d4f3855
chenboshuo <chenboshuo.hi@163.com>
2020-04-08 14:59:55 +0800

    :art: 392ms(17.55%), 13.9MB

--- leetcode/1362_Closest_Divisors/brute_force.py
+++ leetcode/1362_Closest_Divisors/brute_force.py
@@ -3,26 +3,18 @@ class Solution:
         closet = float('inf')
         limit = int((num+2) ** (1/2)) + 1
         for i in range(1, limit):
-            if (num+1) % i == 0:
-                j = (num+1)//i
-                if j-i < closet:
-                    ans_i = i
-                    ans_j = j
-                    closet = j-i
-            if (num+2) % i == 0:
-                j = (num+2)//i
-                if j-i < closet:
-                    ans_i = i
-                    ans_j = j
-                    closet = j-i
+            for delta in (1, 2):
+                if (num+delta) % i == 0:
+                    j = (num+delta)//i
+                    if j-i < closet:
+                        ans_i = i
+                        ans_j = j
+                        closet = j-i
 
         return [ans_i, ans_j]
 
 # https://leetcode.com/submissions/detail/305961255/
 
 
-# 113 / 113 test cases passed.
-# Status: Accepted
-# Runtime: 324 ms
-# Memory Usage: 12.7 MB
-# Submitted: 1 month, 2 weeks ago
+# Runtime: 392 ms, faster than 17.55 % of Python3 online submissions for Closest Divisors.
+# Memory Usage: 13.9 MB, less than 100.00 % of Python3 online submissions for Closest Divisors.
