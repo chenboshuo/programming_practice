9b573d691d9955f54c138695bd8a99e9cc009f9d
chenboshuo <chenboshuo.hi@163.com>
2020-04-13 18:48:09 +0800

    :hankey: overflow

--- leetcode/0525_Contiguous_Array/mycode.c
+++ leetcode/0525_Contiguous_Array/mycode.c
@@ -1,27 +1,45 @@
-int next_n(int* nums, int nums_size, int n, int index){
-  for(++index;index<nums_size;++index){
-    if(nums[index] == n){
-      return index;
+#define INIT -2
+
+int findMaxLength(int* nums, int nums_size) {
+  int d = 0;  // d = m-n, where m in the number of m, n is the number of n
+  int memo[nums_size + 2];  // record the index that memo[index] == d
+  int* p_memo = memo + nums_size / 2 + 1;  // enable the negatve subscription
+  int max_length = 0;                      // storage the max length
+
+  // initialize memo
+  for (int i = 0; i < nums_size + 2; ++i) {
+    memo[i] = INIT;
+  }
+
+  // manage the cases begin with 0th element
+  p_memo[0] = -1;
+
+  for (int i = 0; i < nums_size; ++i) {
+    if (nums[i] == 1) {
+      ++d;
+    } else {
+      --d;
+    }
+
+    if (p_memo[d] != INIT) {
+      int length = i - p_memo[d];
+
+      if (length > max_length) {
+        max_length = length;
+      }
+    } else {
+      p_memo[d] = i;
     }
   }
-  return -1;
+  // return d;
+  return max_length;
 }
 
-int findMaxLength(int* nums, int nums_size){
-  int zero_i = -1, one_i = -1,count=0;
-  while((zero_i = next_n(nums, nums_size,0,zero_i)) != -1 && 
-        (one_i = next_n(nums, nums_size,1,zero_i) != -1)){
-    count+=2;
-  }
-  return count;
-}
 
-// 259 / 555 test cases passed.
-// Status: Wrong Answer
-// Submitted: 3 hours, 20 minutes ago
-// Input:
-// [0,0,1,0,0,0,1,1]
-// Output:
-// 10
-// Expected:
-// 6
+// 6 / 555 test cases passed.
+// Status: Runtime Error
+// Submitted: 15 minutes ago
+// Runtime Error Message:
+// AddressSanitizer: dynamic-stack-buffer-overflow on address 0x7fff9e619908 at pc 0x000000401ae6 bp 0x7fff9e6198b0 sp 0x7fff9e6198a0
+// Last executed input:
+// [1,1,1,1,1,1,1,1]
