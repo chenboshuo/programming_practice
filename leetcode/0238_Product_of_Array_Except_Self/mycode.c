8a7efdc61db4ef9b0c1c995d179cdefef3ef100b
chenboshuo <chenboshuo.hi@163.com>
2020-04-15 18:47:56 +0800

    :fire: remove the use of division.48ms(67.12%), 13.8MB(100%). 移除除法,符合要求

--- leetcode/0238_Product_of_Array_Except_Self/mycode.c
+++ leetcode/0238_Product_of_Array_Except_Self/mycode.c
@@ -2,45 +2,22 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* productExceptSelf(int* nums, int nums_size, int* return_size) {
-  int product = 1;
-  bool has_zero = false;
+  int left_product = 1;
   int* output = (int*)malloc(sizeof(int) * nums_size);
   *return_size = nums_size;
-
   for (int i = 0; i < nums_size; ++i) {
-    if (nums[i] != 0) {
-      product *= nums[i];
-    } else if (has_zero) {
-      for (int j = 0; j < nums_size; ++j) {
-        output[j] = 0;
-      }
-      return output;
-    } else {
-      has_zero = true;
-    }
+    output[i] = left_product;
+    left_product *= nums[i];
   }
-
-  if (has_zero) {
-    for (int i = 0; i < nums_size; ++i) {
-      if (nums[i] != 0) {
-        output[i] = 0;
-      } else {
-        output[i] = product;
-      }
-    }
-    return output;
+  int right_product = 1;
+  for (int i = nums_size - 1; i > -1; --i) {
+    output[i] *= right_product;
+    right_product *= nums[i];
   }
-
-  for (int i = 0; i < nums_size; ++i) {
-    output[i] = product / nums[i];
-  }
-
   return output;
 }
 
-// https://leetcode.com/submissions/detail/325103450/
-// 18 / 18 test cases passed.
-// Status: Accepted
-// Runtime: 36 ms
-// Memory Usage: 13.7 MB
-// Submitted: 1 hour, 18 minutes ago
+// Runtime: 48 ms, faster than 67.12% of C online submissions for Product of Array Except Self.
+// Memory Usage: 13.8 MB, less than 100.00% of C online submissions for Product of Array Except Self.
+
+// https://leetcode.com/submissions/detail/325122370/
