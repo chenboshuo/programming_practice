1b92e70bbe26b621668e85d51adc62b601f31305
chenboshuo <chenboshuo.hi@163.com>
2020-04-15 18:44:54 +0800

    :bug: fix the bugs of zero. 修复了0的处理问题

--- leetcode/0238_Product_of_Array_Except_Self/mycode.c
+++ leetcode/0238_Product_of_Array_Except_Self/mycode.c
@@ -1,25 +1,46 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* productExceptSelf(int* nums, int nums_size, int* return_size){
+int* productExceptSelf(int* nums, int nums_size, int* return_size) {
   int product = 1;
-  
-  for(int i=0;i<nums_size;++i){
-    product *= nums[i];
-  }
-  
-  int *output = (int*)malloc(sizeof(int)*nums_size);
+  bool has_zero = false;
+  int* output = (int*)malloc(sizeof(int) * nums_size);
   *return_size = nums_size;
-  
-  for(int i=0;i<nums_size;++i){
+
+  for (int i = 0; i < nums_size; ++i) {
+    if (nums[i] != 0) {
+      product *= nums[i];
+    } else if (has_zero) {
+      for (int j = 0; j < nums_size; ++j) {
+        output[j] = 0;
+      }
+      return output;
+    } else {
+      has_zero = true;
+    }
+  }
+
+  if (has_zero) {
+    for (int i = 0; i < nums_size; ++i) {
+      if (nums[i] != 0) {
+        output[i] = 0;
+      } else {
+        output[i] = product;
+      }
+    }
+    return output;
+  }
+
+  for (int i = 0; i < nums_size; ++i) {
     output[i] = product / nums[i];
   }
-  
+
   return output;
 }
 
-// https://leetcode.com/submissions/detail/325086933/
-// Runtime Error Message:
-// Line 17: Char 25: runtime error: division by zero (solution.c)
-// Last executed input:
-// [0,0]
+// https://leetcode.com/submissions/detail/325103450/
+// 18 / 18 test cases passed.
+// Status: Accepted
+// Runtime: 36 ms
+// Memory Usage: 13.7 MB
+// Submitted: 1 hour, 18 minutes ago
