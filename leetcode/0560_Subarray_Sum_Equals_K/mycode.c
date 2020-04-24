6eab33b677e096f48c5f01024e4bcd0832be32ca
chenboshuo <chenboshuo.hi@163.com>
2020-04-24 11:22:04 +0800

    :hankey: try to O(n^2).  尝试优化

--- leetcode/0560_Subarray_Sum_Equals_K/mycode.c
+++ leetcode/0560_Subarray_Sum_Equals_K/mycode.c
@@ -1,21 +1,23 @@
 int subarraySum(int* nums, int nums_size, int k) {
-  long int s[nums_size + 1];
-  int* sums = s + 1;
   int count = 0;
-  sums[0] = 0;
   for (int i = 0; i < nums_size; ++i) {
-    sums[i] = sums[i - 1] + nums[i];
-    for (int j = -1; j < i; ++j) {
-      if (sums[i] - sums[j] == k) {
+    int sum = 0;
+    for (int j = i; j < nums_size; ++j) {
+      if ((sum += nums[j]) == k) {
         ++count;
+        continue;
       }
     }
   }
   return count;
 }
 
-// 80 / 80 test cases passed.
-// Status: Accepted
-// Runtime: 488 ms
-// Memory Usage: 6.6 MB
-// Submitted: 1 day, 19 hours ago
+// Wrong Answer
+// Details 
+// Input
+// [0,0,0,0,0,0,0,0,0,0]
+// 0
+// Output
+// 10
+// Expected
+// 55
