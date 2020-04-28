500fd736c91000b65046e6de407b44aa13d0c274
chenboshuo <chenboshuo.hi@163.com>
2020-04-28 14:56:38 +0800

    :hankey: still wrong.

--- leetcode/0221_ Maximal_Square/mycode.c
+++ leetcode/0221_ Maximal_Square/mycode.c
@@ -1,8 +1,10 @@
 #define max(a, b) (((a) > (b)) ? (a) : (b))
 
 int maximalSquare(char** matrix, int matrix_size, int* matrix_col_size) {
+  if (!matrix_size) {
+    return 0;
+  }
   int memo[matrix_size + 1][*matrix_col_size + 1];
-  memo[0][0] = 0;  // for [] case
   for (int i = 0; i < matrix_size + 1; ++i) {
     for (int j = 0; j < *matrix_col_size + 1; ++j) {
       if (i == 0 || j == 0) {
@@ -20,3 +22,11 @@ int maximalSquare(char** matrix, int matrix_size, int* matrix_col_size) {
   int side_length = memo[matrix_size][*matrix_col_size];
   return side_length * side_length;
 }
+
+// 5 / 69 test cases passed.
+// Status: Runtime Error
+// Submitted: 18 hours, 24 minutes ago
+// Runtime Error Message:
+// AddressSanitizer: heap-buffer-overflow on address 0x6020000002ef at pc 0x000000401ff4 bp 0x7fffa258ba50 sp 0x7fffa258ba40
+// Last executed input:
+// [["0","1"]]
