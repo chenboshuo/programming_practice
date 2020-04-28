9cea263f022c1d9f8bdee7e0a1e3238732fe8c4a
chenboshuo <chenboshuo.hi@163.com>
2020-04-28 15:00:17 +0800

    :tada: 12ms(100%),7.2MB

--- leetcode/0221_ Maximal_Square/mycode.c
+++ leetcode/0221_ Maximal_Square/mycode.c
@@ -1,32 +1,28 @@
-#define max(a, b) (((a) > (b)) ? (a) : (b))
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define max(a, b) ((a) > (b) ? (a) : (b))
 
 int maximalSquare(char** matrix, int matrix_size, int* matrix_col_size) {
   if (!matrix_size) {
     return 0;
   }
+  int side_length = 0;
   int memo[matrix_size + 1][*matrix_col_size + 1];
   for (int i = 0; i < matrix_size + 1; ++i) {
     for (int j = 0; j < *matrix_col_size + 1; ++j) {
-      if (i == 0 || j == 0) {
+      if (i == 0 || j == 0 || matrix[i - 1][j - 1] == '0') {
         memo[i][j] = 0;
-      } else if (matrix[i - 1][j - 1] == '1' &&
-                 memo[i][j - 1] == memo[i - 1][j] &&
-                 (memo[i][j - 1] == 0 || (matrix[i - 2][i - 1] == '1') &&
-                                             (matrix[i - 1][i - 2] == '1'))) {
-        memo[i][j] = memo[i - 1][j - 1] + 1;
       } else {
-        memo[i][j] = max(memo[i - 1][j], memo[i][j - 1]);
+        memo[i][j] =
+            min(min(memo[i - 1][j], memo[i][j - 1]), memo[i - 1][j - 1]) + 1;
+        side_length = max(side_length, memo[i][j]);
       }
     }
   }
-  int side_length = memo[matrix_size][*matrix_col_size];
+
   return side_length * side_length;
 }
 
-// 20 / 69 test cases passed.
-// Status: Runtime Error
-// Submitted: 5 hours, 57 minutes ago
-// Runtime Error Message:
-// AddressSanitizer: heap-buffer-overflow on address 0x602000000c94 at pc 0x000000401f9c bp 0x7ffe5be22a20 sp 0x7ffe5be22a10
-// Last executed input:
-// [["0","0","0","1"],["1","1","0","1"],["1","1","1","1"],["0","1","1","1"],["0","1","1","1"]]
+// 69 / 69 test cases passed.
+// Status: Accepted
+// Runtime: 12 ms
+// Memory Usage: 7.2 MB
