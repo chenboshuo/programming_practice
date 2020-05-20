fc314874338ddcd7e8a8516cef103d0ea06ca53e
chenboshuo <chenboshuo.hi@163.com>
2020-05-20 16:43:00 +0800

    :hankey: The content from May leetcoding challenge.

---
+++ leetcode/0402_Remove_K_Digits/mycode.c
@@ -0,0 +1,17 @@
+char * removeKdigits(char * num, int k){
+  int slow = 0;
+  int fast;
+  for(fast=1;num[fast];++fast){
+    if(num[fast] < num[slow] && k){
+      num[slow] = num[fast];
+      --k;
+    }else{
+      num[++slow] = num[fast];
+    }
+  }
+  num[slow+1] = '\0';
+  while(*num == '0' && *(num+1)){
+    ++num;
+  }
+  return num;
+}
