1184496b873aebd2983b14702edb0f88b65a178b
chenboshuo <chenboshuo.hi@163.com>
2020-05-20 16:47:46 +0800

    :hankey: Wrong thinking

--- leetcode/0402_Remove_K_Digits/mycode.c
+++ leetcode/0402_Remove_K_Digits/mycode.c
@@ -1,22 +1,29 @@
 #include <string.h>
 
-char * removeKdigits(char * num, int k){
-  int slow = 0;
+char *removeKdigits(char *num, int k) {
+  // int num_size = strlen(num);
   int fast;
-  for(fast=1;num[fast];++fast){
-    if(num[fast] < num[slow] && k){
-      num[slow] = num[fast];
-      --k;
-    }else{
-      num[++slow] = num[fast];
+  while (k) {
+    int slow = 0;
+    for (fast = 1; num[fast]; ++fast) {
+      if (num[fast] < num[slow] && k) {
+        num[slow] = num[fast];
+        --k;
+      } else {
+        num[++slow] = num[fast];
+      }
     }
+    num[slow + 1] = '\0';
   }
-  num[slow+1-k] = '\0';
-  if(*num == '\0'){
-    return "0";
-  }
-  while(*num == '0' && *(num+1)){
-    ++num;
-  }
+
+  if (*num == '\0') { return "0"; }
+  while (*num == '0' && *(num + 1)) { ++num; }
   return num;
 }
+
+// 2 / 33 test cases passed.
+// Status: Time Limit Exceeded
+// Submitted: 5 days, 5 hours ago
+// Last executed input:
+// "10"
+// 2
