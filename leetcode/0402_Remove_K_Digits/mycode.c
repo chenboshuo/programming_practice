13d44b04bf0e61af1e4ae699e6f87a92795ea070
chenboshuo <chenboshuo.hi@163.com>
2020-05-20 20:37:53 +0800

    :sparkles: Finished by bruteforce

--- leetcode/0402_Remove_K_Digits/mycode.c
+++ leetcode/0402_Remove_K_Digits/mycode.c
@@ -3,19 +3,13 @@
 char *removeKdigits(char *num, int k) {
   int num_size = strlen(num);
   if (k == num_size) { return "0"; }
-  int fast;
   while (k) {
-    int slow = 0;
-    for (fast = 1; fast < num_size; ++fast) {
-      if (num[fast] < num[slow] && k) {
-        num[slow] = num[fast];
-        --k;
-      } else {
-        num[++slow] = num[fast];
-      }
+    int i;
+    for (i = 0; num[i] && num[i] <= num[i + 1]; ++i) { ; }
+    if (num[i]) {
+      --k;
+      for (; num[i]; ++i) { num[i] = num[i + 1]; }
     }
-    num[slow + 1] = '\0';
-    --num_size;
   }
 
   while (*num == '0' && *(num + 1)) { ++num; }
