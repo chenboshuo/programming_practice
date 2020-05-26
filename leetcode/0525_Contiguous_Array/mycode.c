6f915a439a4827e8a0324ac3d7cd88ec5c3c98f0
chenboshuo <chenboshuo.hi@163.com>
2020-05-26 16:29:19 +0800

    :zap: Change the memo size

--- leetcode/0525_Contiguous_Array/mycode.c
+++ leetcode/0525_Contiguous_Array/mycode.c
@@ -1,16 +1,14 @@
 #define INIT -2
 #define max(a, b) ((a) > (b) ? (a) : (b))
 
-int findMaxLength(int* nums, int nums_size) {
+int findMaxLength(int *nums, int nums_size) {
   int d = 0;  // d = m-n, where m in the number of m, n is the number of n
-  int memo[nums_size * 2 + 4];         // record the index that memo[index] == d
-  int* p_memo = memo + nums_size + 2;  // enable the negative subscription
-  int max_length = 0;                  // storage the max length
+  int memo[nums_size * 2 + 1];     // record the index that memo[index] == d
+  int *p_memo = memo + nums_size;  // enable the negative subscription
+  int max_length = 0;              // storage the max length
 
   // initialize memo
-  for (int i = 0; i < nums_size * 2 + 4; ++i) {
-    memo[i] = INIT;
-  }
+  for (int i = 0; i < nums_size * 2 + 1; ++i) { memo[i] = INIT; }
 
   // manage the cases begin with 0th element
   p_memo[0] = -1;
@@ -31,5 +29,6 @@ int findMaxLength(int* nums, int nums_size) {
   return max_length;
 }
 
-// Runtime: 84 ms, faster than 87.50% of C online submissions for Contiguous Array.
-// Memory Usage: 9.9 MB, less than 100.00% of C online submissions for Contiguous Array.
+// /Runtime: 84 ms, faster than 94.08% of C online submissions for Contiguous
+// Array. Memory Usage: 9.9 MB, less than 100.00% of C online submissions for
+// Contiguous Array.issions for Contiguous Array.
