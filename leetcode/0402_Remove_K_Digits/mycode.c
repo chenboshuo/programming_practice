2e12da97c3de95f4efb9f854bb022996057903ee
chenboshuo <chenboshuo.hi@163.com>
2020-05-31 15:56:33 +0800

    :bug: trim the lead 0s.

--- leetcode/0402_Remove_K_Digits/mycode.c
+++ leetcode/0402_Remove_K_Digits/mycode.c
@@ -10,16 +10,16 @@ char *removeKdigits(char *num, int k) {
     ++num;
   }
   ans[ans_p] = '\0';
+  while (*ans == '0' && *(ans + 1)) { ++ans; }
+
   return ans;
 }
 
-// 1 / 33 test cases passed.
-// Status: Wrong Answer
+// Submission Detail
+// 2 / 33 test cases passed.
+// Status: Runtime Error
 // Submitted: 2 days, 5 hours ago
-// Input:
-// "10200"
-// 1
-// Output:
-// "0200"
-// Expected:
-// "200"
+// Runtime Error Message:
+// AddressSanitizer: heap-buffer-overflow on address 0x6020000000f1 at pc
+// 0x000000401bbe bp 0x7ffec8e46570 sp 0x7ffec8e46560 Last executed input: "10"
+// 2
