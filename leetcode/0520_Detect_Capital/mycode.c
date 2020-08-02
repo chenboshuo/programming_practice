72a50442dc55f11c2867905340c8434da88bf31b
chenboshuo <chenboshuo.hi@163.com>
2020-08-02 14:48:29 +0800

    :sparkles: 0ms,5.3MB

--- leetcode/0520_Detect_Capital/mycode.c
+++ leetcode/0520_Detect_Capital/mycode.c
@@ -1,7 +1,16 @@
 #include <stdbool.h>
+
 bool detectCapitalUse(char *word) {
+  if (*word == '\0' || *(word + 1) == '\0') { return true; }
+  bool first = (*word >= 'A' && *word <= 'Z');
+  ++word;
+  bool pre = (*word >= 'A' && *word <= 'Z');
+  ++word;
+  if (pre > first) { return false; }
+
   while (*word) {
-    if (*word < 'A' || *word > 'Z') { return false; }
+    bool cur = (*word >= 'A' && *word <= 'Z');
+    if (cur > first || cur != pre) { return false; }
     ++word;
   }
   return true;
