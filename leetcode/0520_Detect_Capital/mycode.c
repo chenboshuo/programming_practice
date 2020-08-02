963750d83840fc51f00d5c99d07229eaf0b6e350
chenboshuo <chenboshuo.hi@163.com>
2020-08-02 14:47:26 +0800

    :hankey: a wrong thinking.

--- leetcode/0520_Detect_Capital/mycode.c
+++ leetcode/0520_Detect_Capital/mycode.c
@@ -1,7 +1,7 @@
 #include <stdbool.h>
 bool detectCapitalUse(char *word) {
   while (*word) {
-    if (*word < 'A' && *word > 'Z') { return false; }
+    if (*word < 'A' || *word > 'Z') { return false; }
     ++word;
   }
   return true;
