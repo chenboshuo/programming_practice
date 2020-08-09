b01bb6cf9652fb0e45b0e192b5f4b6c8a23db393
chenboshuo <chenboshuo.hi@163.com>
2020-08-09 16:15:15 +0800

    :sparkles: 12ms,5.5MB

--- leetcode/0994_Rotting_Oranges/mycode.c
+++ leetcode/0994_Rotting_Oranges/mycode.c
@@ -1,14 +1,14 @@
 #define FRESH 1
 #define ROTTEN 2
+#define VISITING 3
 #define EMPTY 0
 #define max(a, b) ((a) > (b) ? (a) : (b))
 
 int orangesRotting(int **grid, int gridSize, int *gridColSize) {
-
   void rot(int i, int j, int rotten_time) {
     if (i < 0 || i >= gridSize || j < 0 || j >= *gridColSize ||
-        grid[i][j] == EMPTY ||
-        (grid[i][j] <0  && grid[i][j] > rotten_time)) {
+        grid[i][j] == EMPTY || grid[i][j] == VISITING ||
+        (grid[i][j] < 0 && grid[i][j] > rotten_time)) {
       return;
     }
     if (grid[i][j] == FRESH) {
@@ -16,14 +16,17 @@ int orangesRotting(int **grid, int gridSize, int *gridColSize) {
       // ++rotten;
     } else if (grid[i][j] < 0) {
       grid[i][j] = max(rotten_time, grid[i][j]);
-    }else{ // the orange has rotten
-      grid[i][j] = 0;
+    } else {  // the orange has rotten
+      grid[i][j] = VISITING;
     }
 
     rot(i + 1, j, rotten_time - 1);
     rot(i - 1, j, rotten_time - 1);
     rot(i, j + 1, rotten_time - 1);
     rot(i, j - 1, rotten_time - 1);
+    if (grid[i][j] == VISITING) {  // restore
+      grid[i][j] = ROTTEN;
+    }
   }
 
   for (int i = 0; i < gridSize; ++i) {
@@ -43,10 +46,8 @@ int orangesRotting(int **grid, int gridSize, int *gridColSize) {
   return max_minute;
 }
 
-// Submission Result: Wrong Answer
-// Input:
-// [[2,2,2,1,1]]
-// Output:
-// 4
-// Expected:
-// 2
+// 303 / 303 test cases passed.
+// Status: Accepted
+// Runtime: 12 ms
+// Memory Usage: 5.5 MB
+// Submitted: 0 minutes ago
