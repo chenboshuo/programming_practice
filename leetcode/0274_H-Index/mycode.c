e2238e055b64528ca089fba52f8cd9df61e9849a
chenboshuo <chenboshuo.hi@163.com>
2020-08-12 13:37:18 +0800

    :sparkles: 4ms, 5.8MB

--- leetcode/0274_H-Index/mycode.c
+++ leetcode/0274_H-Index/mycode.c
@@ -1,25 +1,21 @@
 #include <stdlib.h>
 int reverse(const void *a, const void *b) { return *(int *)b - *(int *)a; }
 int hIndex(int *citations, int citationsSize) {
-  if (citationsSize == 0 || (citationsSize == 1 && citations[0] == 0)) { return 0; }
+  if (citationsSize == 0) { return 0; }
   qsort(citations, citationsSize, sizeof(int), reverse);
   int left = 0;
   int right = citationsSize - 1;
-  while (left < right) {
+
+  while (left <= right) {
     int mid = left + ((right - left) >> 1);
     if (citations[mid] >= mid + 1) {
-      left = mid;
+      left = mid + 1;
     } else {
       right = mid - 1;
     }
   }
-  return left + 1;
+  if(citations[0] != 0){
+    return right+ 1;
+  }
+  return 0;
 }
-
-// Submission Result: Wrong Answer 
-// Input:
-// [0,0]
-// Output:
-// 1
-// Expected:
-// 0
