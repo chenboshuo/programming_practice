d71b1287d7b1ecd4b1aed7629cef91465a28eb3f
chenboshuo <chenboshuo.hi@163.com>
2020-08-12 11:30:47 +0800

    :hankey: forget null string.

---
+++ leetcode/0274_H-Index/mycode.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+int reverse(const void *a, const void *b) { return *(int *)b - *(int *)a; }
+int hIndex(int *citations, int citationsSize) {
+  if (citationsSize == 0) { return 0; }
+  qsort(citations, citationsSize, sizeof(int), reverse);
+  int left = 0;
+  int right = citationsSize - 1;
+  while (left < right) {
+    int mid = left + ((right - left) >> 1);
+    if (citations[mid] >= mid + 1) {
+      left = mid;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return left + 1;
+}
+
+// Submission Result: Wrong Answer
+// Input:
+// []
+// Output:
+// 1
+// Expected:
+// 0
