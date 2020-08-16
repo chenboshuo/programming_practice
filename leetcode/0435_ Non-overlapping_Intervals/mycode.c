5ccf50beec6e485a0d6ac475b5b80b919a32ac6e
chenboshuo <chenboshuo.hi@163.com>
2020-08-16 14:32:37 +0800

    :bug: fix the bugs of error sort func.

--- leetcode/0435_ Non-overlapping_Intervals/mycode.c
+++ leetcode/0435_ Non-overlapping_Intervals/mycode.c
@@ -1,6 +1,6 @@
 #include <stdlb.h>
 int by_ends(const void *a, const void *b) {
-  return ((int *)a)[1] - ((int *)b)[1];
+  return (*(int **)a)[1] - (*(int **)b)[1];
 }
 
 int eraseOverlapIntervals(int **intervals, int intervalsSize,
@@ -8,7 +8,7 @@ int eraseOverlapIntervals(int **intervals, int intervalsSize,
   qsort(intervals, intervalsSize, sizeof(int **), by_ends);
 
   int choose = 0;
-  int cover_begin = 0;
+  // int cover_begin = 0;
   int cover_end = 0;
   for(int i=0;i<intervalsSize;++i){
     if(intervals[i][0] >= cover_end){
@@ -19,10 +19,10 @@ int eraseOverlapIntervals(int **intervals, int intervalsSize,
   return intervalsSize - choose;
 }
 
-// https://leetcode.com/submissions/detail/381604750/?from=/explore/challenge/card/august-leetcoding-challenge/551/week-3-august-15th-august-21st/3425/
-// Input:
-// [[1,100],[11,22],[1,11],[2,12]]
+// https://leetcode.com/submissions/detail/381606424/?from=/explore/challenge/card/august-leetcoding-challenge/551/week-3-august-15th-august-21st/3425///
+// Input: Input:
+// [[1,2],[2,3],[3,4],[-100,-2],[5,7]]
 // Output:
-// 3
+// 1
 // Expected:
-// 2
+// 0
