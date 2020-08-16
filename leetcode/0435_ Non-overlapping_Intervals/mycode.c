58803b6ec9c5ba93da4298c0a3b888c690fc6830
chenboshuo <chenboshuo.hi@163.com>
2020-08-16 14:33:11 +0800

    :bug: handle the negative cases. 12ms(100%),7.1MB

--- leetcode/0435_ Non-overlapping_Intervals/mycode.c
+++ leetcode/0435_ Non-overlapping_Intervals/mycode.c
@@ -1,3 +1,4 @@
+#include <limit.h>
 #include <stdlb.h>
 int by_ends(const void *a, const void *b) {
   return (*(int **)a)[1] - (*(int **)b)[1];
@@ -9,9 +10,9 @@ int eraseOverlapIntervals(int **intervals, int intervalsSize,
 
   int choose = 0;
   // int cover_begin = 0;
-  int cover_end = 0;
-  for(int i=0;i<intervalsSize;++i){
-    if(intervals[i][0] >= cover_end){
+  int cover_end = INT_MIN;
+  for (int i = 0; i < intervalsSize; ++i) {
+    if (intervals[i][0] >= cover_end) {
       ++choose;
       cover_end = intervals[i][1];
     }
@@ -19,10 +20,7 @@ int eraseOverlapIntervals(int **intervals, int intervalsSize,
   return intervalsSize - choose;
 }
 
-// https://leetcode.com/submissions/detail/381606424/?from=/explore/challenge/card/august-leetcoding-challenge/551/week-3-august-15th-august-21st/3425///
-// Input: Input:
-// [[1,2],[2,3],[3,4],[-100,-2],[5,7]]
-// Output:
-// 1
-// Expected:
-// 0
+// https://leetcode.com/submissions/detail/381607103/?from=/explore/challenge/card/august-leetcoding-challenge/551/week-3-august-15th-august-21st/3425///
+// Input: Input: 18 / 18 test cases passed. Status: Accepted Runtime: 12 ms
+// Memory Usage: 7.1 MB
+// Submitted: 0 minutes ago
