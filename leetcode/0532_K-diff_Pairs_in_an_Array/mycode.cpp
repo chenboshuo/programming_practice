2207a831f6bcaee28bd448de78780a472a7d7b38
chenboshuo <chenboshuo.hi@163.com>
2020-10-03 20:13:20 +0800

    :zap: 40ms(99.22%),14.3MB,using unordered map

--- leetcode/0532_K-diff_Pairs_in_an_Array/mycode.cpp
+++ leetcode/0532_K-diff_Pairs_in_an_Array/mycode.cpp
@@ -1,38 +1,38 @@
-#include <map>
+#include <unordered_map>
 #include <vector>
 using namespace std;
 class Solution {
  public:
   int findPairs(vector<int> &nums, int k) {
-    map<int, int> match;
+    unordered_map<int, int> match;
     int count = 0;
 
     // create the map
-    for(int x:nums){
-      match[x-k] += 1;
-    }
+    for (int x : nums) { match[x - k] += 1; }
 
-    if(k != 0){
-      for(int x: nums){
-        if(match[x]){
+    if (k != 0) {
+      for (int x : nums) {
+        if (match[x]) {
           ++count;
           match[x] = 0;
         }
       }
-    }else{
-      for(int x:nums){
-        if(match[x] > 1){
+    } else {
+      for (int x : nums) {
+        if (match[x] > 1) {
           ++count;
           match[x] = 0;
         }
       }
     }
-
 
     return count;
   }
 };
 
-// https://leetcode.com/submissions/detail/403889457/?from=/explore/challenge/card/october-leetcoding-challenge/559/week-1-october-1st-october-7th/3482/
-// Runtime: 92 ms
-// Memory Usage: 14.8 MB
+// https://leetcode.com/submissions/detail/403890100/?from=/explore/challenge/card/october-leetcoding-challenge/559/week-1-october-1st-october-7th/3482/
+// 59 / 59 test cases passed.
+// Status: Accepted
+// Runtime: 40 ms
+// Memory Usage: 14.3 MB
+// Submitted: 0 minutes ago
