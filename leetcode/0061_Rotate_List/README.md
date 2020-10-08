c9afb3c20a9fabc3f76d607303133533eb2ecdf9
chenboshuo <chenboshuo.hi@163.com>
2020-10-08 12:46:15 +0800

    :tada: 4ms(69.57%),6.1mb

---
+++ leetcode/0061_Rotate_List/README.md
@@ -0,0 +1,26 @@
+# 61. Rotate List
+
+[leetcode.com](https://leetcode.com/problems/rotate-list/)
+
+Given a linked list, rotate the list to the right by _k_ places, where _k_ is non-negative.
+
+**Example 1:**
+
+```text
+Input: 1->2->3->4->5->NULL, k = 2
+Output: 4->5->1->2->3->NULL
+Explanation:
+rotate 1 steps to the right: 5->1->2->3->4->NULL
+rotate 2 steps to the right: 4->5->1->2->3->NULL
+
+
+```
+
+**Example 2:**
+
+```text
+2->0->1->NULL
+0->1->2->NULL
+2->0->1->NULL
+
+```
--- leetcode/0061_Rotate_List/mycode.c
+++ leetcode/0061_Rotate_List/mycode.c
@@ -12,15 +12,19 @@ struct ListNode {
 };
 
 struct ListNode *rotateRight(struct ListNode *head, int k) {
-  if (!head || k == 0) { return head; }
+  if (!head) { return head; }
   struct ListNode *fast = head, *pre_fast = head;
-  while (k--) {
-    if (fast) {
-      pre_fast = fast;
-      fast = fast->next;
-    } else {
-      fast = head->next;
-    }
+
+  // get the length of linked list
+  int length = 0;
+  for (struct ListNode *i = head; i; i = i->next) { ++length; }
+  k %= length;
+  if (k == 0) { return head; }
+
+  // find the new head
+  while (k-- > 0) {
+    pre_fast = fast;
+    fast = fast->next;
   }
   struct ListNode *slow = head, *pre_slow = NULL;
   while (fast) {
@@ -29,11 +33,15 @@ struct ListNode *rotateRight(struct ListNode *head, int k) {
     fast = fast->next;
     slow = slow->next;
   }
-  if(pre_slow){
+  if (pre_slow) {
     pre_fast->next = head;
     pre_slow->next = NULL;
   }
   return slow;
 }
-// 231 / 231 test cases passed, but took too long.
-// Status: Time Limit Exceeded
+
+// 231 / 231 test cases passed.
+// Status: Accepted
+// Runtime: 4 ms
+// Memory Usage: 6.1 MB
+// Submitted: 0 minutes ago
