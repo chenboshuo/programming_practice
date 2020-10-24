1411d9222017211988005afe1b8b6508a42e1d95
chenboshuo <chenboshuo.hi@163.com>
2020-10-24 16:59:02 +0800

    :beers: new algorithm, but still wrong.

--- leetcode/0948_Bag_of_Tokens/mycode.c
+++ leetcode/0948_Bag_of_Tokens/mycode.c
@@ -1,25 +1,32 @@
 #include <stdlib.h>
 #define max(a, b) ((a > b) ? (a) : (b))
 int by_value(const void *a, const void *b) { return *(int *)a - *(int *)b; }
-int bag_of_tokens_score(int *tokens, int left, int right, int p) {
-  if (p < 0) { return -1; }
-  if (left > right) { return 0; }
-  if (left == right) { return p >= tokens[left]; }
-  int face_down = 0;
-  if (p >= tokens[left]) {
-    face_down = bag_of_tokens_score(
-        tokens, left + 1, right - 1,
-        p + tokens[right] - tokens[left]);  // use the least to face down
-  }
-  int face_up =
-      bag_of_tokens_score(tokens, left + 1, right, p - tokens[left]) + 1;
-  return max(face_down, face_up);
-}
+
 int bagOfTokensScore(int *tokens,  //
                      int tokensSize, int P) {
   qsort(tokens, tokensSize, sizeof(int), by_value);
-  return bag_of_tokens_score(tokens, 0, tokensSize - 1, P);
+  int up_max_score =
+      -1;  // the point nearest to P=0 from face up_max_score direction
+  int down_max_score = -1;
+  int score = 0;
+  for (int i = 0; i < tokensSize; ++i) {
+    P -= tokens[i];
+    ++score;
+    if (P < 0 && up_max_score == -1) { up_max_score = score - 1; }
+  }
+  if (up_max_score > 0) {
+    for (int i = tokensSize - 1; i >= 0; --i) {
+      if (P >= 0 && down_max_score == -1) { down_max_score = score; }
+      P += 2 * tokens[i];
+      score -= 2;
+    }
+  }
+  return max(up_max_score, down_max_score);
 }
-// time limmit exceed
-// [2897,6861,2070,5292,3402,9784,9718,2089,5660,3294,9685,9245,5861,7200,6813,3533,9163,8994,3306,7473,90,8163,5648,9523,3631,6257,3230,7827,6007,9874,10,1407,436,1258,9293,9486,4804,9466,8183,7786,7472,1876,5488,4238,9497,1738,1698,6588,1574,1100]
-// 5039
+// Input:
+// []
+// 85
+// Output:
+// -1
+// Expected:
+// 0
