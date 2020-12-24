e9ce95ea187e264662d1d5709f569d4bf631c845
chenboshuo <chenboshuo.hi@163.com>
2020-12-24 21:20:37 +0800

    :zap: update the bit operation

--- leetcode/1680_Concatenation_of_Consecutive_Binary_Numbers/shift.c
+++ leetcode/1680_Concatenation_of_Consecutive_Binary_Numbers/shift.c
@@ -2,16 +2,16 @@
 
 int concatenatedBinary(int n) {
   long ans = 0;
+  int shift = 0;
   for (int i = 1; i <= n; ++i) {
-    int len = 0;
-    for (int j = 1; j <=i; j <<= 1) { ++len; }
-    // ans += (((ans << len) % MOD) + i) % MOD;
+    if ((i & (i - 1)) == 0) { ++shift; }
+    ans = ((ans << shift) | i) % MOD;
     // printf("%i %d %d\n",i,len,ans);
-    ans = ((ans << len) + i);
+    // ans = ((ans << len) + i);
   }
   return ans;
 }
 
-// Runtime: 68 ms, faster than 50.00% of C online submissions for Concatenation
+// Runtime: 20 ms, faster than 94.44% of C online submissions for Concatenation
 // of Consecutive Binary Numbers. Memory Usage: 5.5 MB, less than 83.33% of C
 // online submissions for Concatenation of Consecutive Binary Numbers.
