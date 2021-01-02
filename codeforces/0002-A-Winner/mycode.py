36fc58cbf37203e7314f764dfdb9808eb315f7c3
chenboshuo <chenboshuo.hi@163.com>
2021-01-02 11:11:30 +0800

    :poop: using new recording method.

--- codeforces/0002-A-Winner/mycode.py
+++ codeforces/0002-A-Winner/mycode.py
@@ -1,49 +1,26 @@
 from collections import defaultdict
 count = int(input())
 record = defaultdict(int)
-round_memo = dict()
+# round_memo = dict()
+winner_score = 0
 for round_ in range(count):
     name,score = input().split()
     score = int(score)
     record[name] += score
-    if score > 0:
-        round_memo[name] = round_
+    # if score > 0:
+        # round_memo[name] = round_
+    if record[name] > winner_score:
+        winner = name
+        winner_score = record[name]
 
-winner_score = 0
-winner_round = count
-for name,score in record.items():
-    if score > winner_score:
-        winner = name
-        winner_score = score
-        winner_round = round_memo[name]
-    elif score == winner_score and round_memo[name] < winner_round:
-        winner = name
-        winner_round = round_memo[name]
-        winner_score = score
+# winner_round = count
+# for name,score in record.items():
+    # if score > winner_score:
+        # winner = name
+    #     # winner_score = score
+    #     winner_round = round_memo[name]
+    # elif score == winner_score and round_memo[name] < winner_round:
+    #     winner = name
+    #     winner_round = round_memo[name]
+    #     winner_score = score
 print(winner)
-
-
-# Test: #6, time: 280 ms., memory: 288 KB, exit code: 0, checker exit code: 1, verdict: WRONG_ANSWER
-# Input
-# 15
-# aawtvezfntstrcpgbzjbf 681
-# zhahpvqiptvksnbjkdvmknb -74
-# aawtvezfntstrcpgbzjbf 661
-# jpdwmyke 474
-# aawtvezfntstrcpgbzjbf -547
-# aawtvezfntstrcpgbzjbf 600
-# zhahpvqiptvksnbjkdvmknb -11
-# jpdwmyke 711
-# bjmj 652
-# aawtvezfntstrcpgbzjbf -1000
-# aawtvezfntstrcpgbzjbf -171
-# bjmj -302
-# aawtvezfntstrcpgbzjbf 961
-# zhahpvqiptvksnbjkdvmknb 848
-# bjmj -735
-# Output
-# jpdwmyke
-# Answer
-# aawtvezfntstrcpgbzjbf
-# Checker Log
-# wrong answer 1st words differ - expected: 'aawtvezfntstrcpgbzjbf', found: 'jpdwmyke'
