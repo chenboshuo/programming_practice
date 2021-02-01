ee495a3be00481d8eeae2387d3ea9d0489901c0d
chenboshuo <chenboshuo.hi@163.com>
2021-02-01 16:37:20 +0800

    :hankey:still wrong.

--- codeforces/0006C-Alice, Bob-and-Chocolate/mycode.py
+++ codeforces/0006C-Alice, Bob-and-Chocolate/mycode.py
@@ -1,51 +1,30 @@
 import sys
+import logging
+from itertools import accumulate
+from bisect import bisect_left
+
+logging.root.setLevel(logging.INFO)
+
 num = int(sys.stdin.readline())
+if num == 1:
+    print(1,0)
+    exit(0)
 times = list(map(int,sys.stdin.readline().strip().split()))
-left = 0
-right = num-1
+# left = 0
+# right = num-1
+series = list(accumulate(times))
+logging.info(f"series={series}")
+total = series[-1]
+alice = bisect_left(series,total/2+1)
+print(alice,num-alice)
 
-
-def get(i):
-    t = times[i]
-    times[i] = 0
-    return t
-
-
-
-while left<right:
-    a = get(left)
-    b = get(right)
-    time_pass = min(a,b)
-    remain_a = a - time_pass
-    remain_b = b - time_pass
-    # print(times)
-    if remain_a == 0:
-        left += 1
-    else:
-        times[left] = remain_a
-
-    if remain_b == 0:
-        right -= 1
-    else:
-        times[right] = remain_b
-
-# if times[right] != 0:
-#     alice_eating = left
-# else:
-#     alice_eating = left+1
-alice_eating = left
-
-print(alice_eating,num-alice_eating)
-
-
-# Time: 248 ms, memory: 0 KB
-# Verdict: WRONG_ANSWER
+# Test: #7, time: 310 ms., memory: 5940 KB, exit code: 0, checker exit code: 1, verdict: WRONG_ANSWER
 # Input
-# 1
-# 1
-# Participant's output
-# 0 1
-# Jury's answer
-# 1 0
-# Checker comment
+# 2
+# 8 2
+# Output
+# 0 2
+# Answer
+# 1 1
+# Checker Log
 # wrong answer 1st numbers differ - expected: '1', found: '0'
