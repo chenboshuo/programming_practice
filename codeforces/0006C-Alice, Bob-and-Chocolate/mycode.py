fb6ea3e1ea3c24be14acbd5ca83c60481f8acac2
chenboshuo <chenbohuo.hi@163com>
2021-02-13 17:37:08 +0800

    :sparkles: 280ms,8100KB.

--- codeforces/0006C-Alice, Bob-and-Chocolate/mycode.py
+++ codeforces/0006C-Alice, Bob-and-Chocolate/mycode.py
@@ -11,9 +11,9 @@ time_right = 0
 while left < right:
     time_left += times[left]
     left += 1
-    while right >= 0  and time_right + times[right] <= time_left:
-        right -= 1
+    while right >= 0  and time_right < time_left:
         time_right += times[right]
+        right -= 1
  
  
 alice_eating = left + bool((left == right) and (time_left <= time_right))
@@ -21,13 +21,3 @@ print(alice_eating,num-alice_eating)
  
  
 
-# Test: #8, time: 186 ms., memory: 0 KB, exit code: 0, checker exit code: 1, verdict: WRONG_ANSWER
-# Input
-# 3
-# 6 8 4
-# Output
-# 2 1
-# Answer
-# 1 2
-# Checker Log
-# wrong answer 1st numbers differ - expected: '1', found: '2'
