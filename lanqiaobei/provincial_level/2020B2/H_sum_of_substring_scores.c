8c70de46172e1fae83a6a5e1b32a87ba8cff2e29
chenboshuo <chenboshuo.hi@163.com>
2021-02-24 16:26:41 +0800

    :poop: H 子串分值和(难以通过所有用例)

---
+++ lanqiaobei/provincial_level/2020B2/H_sum_of_substring_scores.c
@@ -0,0 +1,72 @@
+// ## H 子串分值和
+// 时间限制: 1.0s 内存限制: 256.0MB 本题总分：20 分
+
+// ### 问题描述
+// 对于一个字符串 `S`，
+// 我们定义 `S` 的分值 `f(S)`
+// 为 S 中出现的不同的字符个数。
+// 例如 `f(”aba”) = 2`，`f(”abc”) = 3`,
+// `f(”aaa”) = 1`。
+// 现在给定一个字符串`S [0…n − 1]`（长度为 n），
+// 请你计算对于所有 `S` 的非空子串 `S [i… j]
+// (0 ≤ i ≤ j < n)`，`f(S [i… j])` 的和是多少。
+
+// ### 输入格式
+// 输入一行包含一个由小写字母组成的字符串 S。
+
+// ### 输出格式
+// 输出一个整数表示答案。
+
+// ### 样例输入
+// ```plt
+// ababc
+// ```
+
+// ### 样例输出
+// ```plt
+// 28
+// ```
+
+// ### 样例说明
+// 子串   f值
+// a      1
+// ab     2
+// aba    2
+// abab   2
+// ababc  3
+//  b     1
+//  ba    2
+//  bab   2
+//  babc  3
+//   a    1
+//   ab   2
+//   abc  3
+//    b   1
+//    bc  2
+//     c  1
+// ```
+#include<stdio.h>
+
+int main(){
+  char s[100001];
+  scanf("%s",s);
+  int left = 0;
+  long long total = 0;
+  while(s[left]){
+    int right = left;
+    int score = 0;
+    int record[26] = {0};
+    while(s[right]){
+      if(!record[s[right] - 'a']){
+        record[s[right] - 'a'] = 1;
+        ++score;
+      }
+      total += score;
+      ++right;
+    }     
+    ++left;
+  }
+  printf("%lld\n",total);
+
+  return 0;
+}
---
+++ lanqiaobei/provincial_level/2020B2/tests/test_H.py
@@ -0,0 +1,16 @@
+import test as t
+t.name = "H_sum_of_substring_scores"
+
+t1='''ababc'''
+
+o1='''28'''
+
+t.test(t1,o1)
+
+t2 = 'a' * 100
+o2 = str(101*100//2)
+t.test(t2,o2,long_input=True)
+
+t3 = 'a' * 100000
+o3 = str(100001*100000//2)
+t.test(t3,o3,long_input=True)
