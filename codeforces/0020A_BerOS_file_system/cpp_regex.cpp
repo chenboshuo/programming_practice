b661c30810b5ab1b5caef3ffdf126672d0d88968
chenboshuo <chenboshuo.hi@163.com>
2022-01-05 10:34:22 +0800

    :zap: cpp solution with (30ms,200KB)

--- codeforces/0020A_BerOS_file_system/cpp_regex.cpp
+++ codeforces/0020A_BerOS_file_system/cpp_regex.cpp
@@ -1,30 +1,24 @@
-#include <string>
 #include <iostream>
-#include <regex>
 #include <iterator>
+#include <regex>
+#include <string>
 using namespace std;
 
-int main(){
+int main() {
   string raw;
   cin >> raw;
   regex filename_pattern_regex("(\\w+)");
-  auto begin_path = sregex_iterator(raw.begin(),raw.end(),filename_pattern_regex);
-  auto end_path = sregex_iterator(); 
-  for(auto p = begin_path;p!=end_path;++p){
+  auto begin_path =
+      sregex_iterator(raw.begin(), raw.end(), filename_pattern_regex);
+  auto end_path = sregex_iterator();
+  if (begin_path == end_path) { cout << "/"; }
+  for (auto p = begin_path; p != end_path; ++p) {
     cout << '/' << p->str();
-  }//usr///local//nginx/sbin
+  }  // usr///local//nginx/sbin
   cout << "\n";
 }
 
 /**
- * 
- * Test: #4, time: 0 ms., memory: 136 KB, exit code: 0, checker exit code: 2, verdict: WRONG_ANSWER
- * Input
- * /
- * Output
- * 
- * Answer
- * /
- * Checker Log
- * wrong output format Unexpected end of file - token expected
-**/
+ *
+ * 30 ms	200 KB
+ **/
