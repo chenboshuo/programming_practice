70f7bd81cb64950d08d127ee4e7019d53f3c897b
chenboshuo <chenboshuo.hi@163.com>
2022-08-08 16:46:13 +0800

    :beers: 5032ms

--- leetcode/0300_Longest_Increasing_Subsequence/dp.py
+++ leetcode/0300_Longest_Increasing_Subsequence/dp.py
@@ -1,12 +1,14 @@
-from collections import defaultdict
+# reference: https://leetcode.com/problems/longest-increasing-subsequence/discuss/1326308/C%2B%2BPython-DP-Binary-Search-BIT-Solutions-Picture-explain-O(NlogN)
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        dp = [0,1]
-        for i in range(1,len(nums)):
-            new_ans = max(
-                dp[-j] + int(nums[i] > nums[i-j])
-                for j in range(1,i+1)
-            )
-            dp.append(new_ans)
-        return dp[len(nums)]
+        dp = [1] * len(nums)
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j]+1)
+        return max(dp)
+
+# https://leetcode.com/submissions/detail/768261405/
+# Runtime: 5032 ms, faster than 46.52% of Python3 online submissions for Longest Increasing Subsequence.
+# Memory Usage: 14.3 MB, less than 47.45% of Python3 online submissions for Longest Increasing Subsequence.
