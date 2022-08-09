7112403989ec8a39ec77d068dfba6aa728455798
chenboshuo <chenboshuo.hi@163.com>
2022-08-09 08:57:07 +0800

    :hankey: still wrong.

--- leetcode/0823_Binary_Trees_With_Factors/dp.py
+++ leetcode/0823_Binary_Trees_With_Factors/dp.py
@@ -5,20 +5,23 @@ class Solution:
         for num in arr:
             subtree = 1
             for pre in tree_num:
-                if num % pre == 0 and num // pre in tree_num:
-                    subtree += tree_num[pre]
+                right = num // pre
+                if num % pre == 0 and right in tree_num and pre <= right:
+                    new_term = tree_num[pre] * tree_num[right]
+                    if pre != right:
+                        new_term *= 2
+                    subtree += new_term
             tree_num[num] = subtree
         return sum(tree_num.values())
 
+# https://leetcode.com/submissions/detail/768871730/
 
-# https://leetcode.com/submissions/detail/768863847/
-# Submission Detail
-# 32 / 48 test cases passed.
+# 42 / 48 test cases passed.
 # Status: Wrong Answer
-# Submitted: 13 minutes ago
+# Submitted: 0 minutes ago
 # Input:
-# [18, 3, 6, 2]
+# [46,144,5040,4488,544,380,4410,34,11,5,3063808,5550,34496,12,540,28,18,13,2,1056,32710656,31,91872,23,26,240,18720,33,49,4,38,37,1457,3,799,557568,32,1400,47,10,20774,1296,9,21,92928,8704,29,2162,22,1883700,49588,1078,36,44,352,546,19,523370496,476,24,6000,42,30,8,16262400,61600,41,24150,1968,7056,7,35,16,87,20,2730,11616,10912,690,150,25,6,14,1689120,43,3128,27,197472,45,15,585,21645,39,40,2205,17,48,136]
 # Output:
-# 10
+# 1509730804
 # Expected:
-# 12
+# 509730797
